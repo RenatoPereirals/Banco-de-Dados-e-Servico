e10e0ff4af447f45ea7ac83d4bff4f905f1aa06f
RenatoPereirals <renatopreirals@gmail.com>
2024-01-02 12:30:39 -0300

    Modificações na classe Bsd

--- src/Bsd.Domain/Entities/Bsd.cs
+++ src/Bsd.Domain/Entities/Bsd.cs
@@ -1,7 +1,37 @@
-﻿namespace Bsd.Domain.Entities
+﻿using System.Collections.ObjectModel;
+using System.Data.Common;
+using Bsd.Domain.Enums;
+
+namespace Bsd.Domain.Entities
 {
     public class Bsd
     {
+        public string BsdNumber { get; }
+        public IEnumerable<Employee> Employee { get; }
+        public DateTime DateService { get; }
+        public DateTime StartTime { get; set; }
+        public DateTime EndTime { get; set; }
+        public TypeDay TypeDay { get; }
+
+        public Bsd(string bsdNumber, IEnumerable<Employee> employees, DateTime dateService)
+        {
+            ValidateInput(bsdNumber, employees, dateService);
+            BsdNumber = bsdNumber;
+            Employee = employees;
+            DateService = dateService;
+        }
+
+        private static void ValidateInput(string bsdNumber, IEnumerable<Employee> employees, DateTime dateService)
+        {
+            if (string.IsNullOrEmpty(bsdNumber))
+            {
+                throw new ArgumentException("Número BSD inválido", nameof(bsdNumber));
+            }
+            if(employees == null)
+            {
+                throw new ArgumentException("Deve conter um número de matrícula do funcionário", nameof(employees));
+            }
+        }
 
     }
 }
