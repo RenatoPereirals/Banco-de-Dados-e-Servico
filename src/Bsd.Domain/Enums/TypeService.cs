9ba9d9d9c4d28745bfd484764f92875e3d99ee26
RenatoPereirals <renatopreirals@gmail.com>
2024-01-02 12:29:43 -0300

    Criação do enum tipo de serviço

--- src/Bsd.Domain/Enums/TypeService.cs
+++ src/Bsd.Domain/Enums/TypeService.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace Bsd.Domain.Enums
+﻿namespace Bsd.Domain.Enums
 {
     public enum TypeService
     {
