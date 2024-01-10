2bea1e1a1d4f66a53d6c827666dc6fe9fef53413
RenatoPereirals <renatopreirals@gmail.com>
2024-01-10 16:23:04 -0300

    Refatoramento do teste EmployeeConstructor_ShouldCreateInstance (Remoção do mock desnecesário)

--- test/Domain/Entities/EmployeeTest.cs
+++ test/Domain/Entities/EmployeeTest.cs
@@ -1,6 +1,5 @@
 using Bsd.Domain.Entities;
 using Bsd.Domain.Enums;
-using Moq;
 
 namespace test.Domain.Entities
 {
@@ -10,21 +9,16 @@ namespace test.Domain.Entities
         public void EmployeeConstructor_ShouldCreateInstance()
         {
             // Arrange
-            var mockRubric = new Mock<Rubric>();
-
-            mockRubric.Setup(r => r.Code).Returns("Valor1");
-            var listRubrics = new List<Rubric> { mockRubric.Object };
-
-            Employee employee = new("3782", ServiceType.P140, listRubrics);
+            Employee employee = new("3782", ServiceType.P140, new List<Rubric>());
 
             // Act
             var typeServiceString = employee.ServiceType.ToString();
+            var rubricsList = new List<Rubric>();
 
             // Assert
             Assert.Equal("P140", typeServiceString);
             Assert.Equal(6, employee.Digit);
-            Assert.NotNull(employee.Bsd);
-            Assert.Single(employee.Rubrics); 
+            Assert.Equal(rubricsList, employee.Rubrics);
         }
     }
 }
