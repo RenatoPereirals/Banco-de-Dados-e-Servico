e43a305f996a4f58cb48188f410b4ccf47dd10e4
RenatoPereirals <renatopreirals@gmail.com>
2024-01-10 09:15:59 -0300

    Delete do IEmployeeRepository

--- src/Bsd.Domain/Repository/IEmployeeRepository.cs
+++
@@ -1,9 +0,0 @@
-using Bsd.Domain.Entities;
-
-namespace Bsd.Domain.Repository
-{
-    public interface IEmployeeRepository
-    {
-        Task<Employee> GetEmployeeByRegistrationAsync(string employeeId);
-    }
-}
