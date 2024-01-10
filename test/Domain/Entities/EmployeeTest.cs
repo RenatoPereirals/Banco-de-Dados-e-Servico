ff19d6da68bbba2174d6af361c4896e83e75c87b
RenatoPereirals <renatopreirals@gmail.com>
2024-01-10 15:57:06 -0300

    Atualização do teste EmployeeConstructor_ShouldCreateInstance para receber um Mock no lugar de uma Rubric

--- test/Domain/Entities/EmployeeTest.cs
+++ test/Domain/Entities/EmployeeTest.cs
@@ -1,5 +1,6 @@
 using Bsd.Domain.Entities;
 using Bsd.Domain.Enums;
+using Moq;
 
 namespace test.Domain.Entities
 {
@@ -9,15 +10,21 @@ namespace test.Domain.Entities
         public void EmployeeConstructor_ShouldCreateInstance()
         {
             // Arrange
-            Employee e = new("3782", ServiceType.P140);
+            var mockRubric = new Mock<Rubric>();
+
+            mockRubric.Setup(r => r.Code).Returns("Valor1");
+            var listRubrics = new List<Rubric> { mockRubric.Object };
+
+            Employee employee = new("3782", ServiceType.P140, listRubrics);
 
             // Act
-            var typeServiceString = e.ServiceType.ToString();
+            var typeServiceString = employee.ServiceType.ToString();
 
             // Assert
             Assert.Equal("P140", typeServiceString);
-            Assert.Equal(6, e.Digit);
-            Assert.NotNull(e.Bsd);
+            Assert.Equal(6, employee.Digit);
+            Assert.NotNull(employee.Bsd);
+            Assert.Single(employee.Rubrics); 
         }
     }
 }
