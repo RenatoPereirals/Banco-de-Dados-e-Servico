bea2f9e6d6b295e56b268033fd09b2dc6df63b18
RenatoPereirals <renatopreirals@gmail.com>
2024-01-10 09:16:27 -0300

    Criação da propriedade Rubrics

--- src/Bsd.Domain/Entities/Bsd.cs
+++ src/Bsd.Domain/Entities/Bsd.cs
@@ -5,6 +5,7 @@ namespace Bsd.Domain.Entities
     {
         public string BsdNumber { get; }
         public IEnumerable<Employee> Employee { get; }
+        public IEnumerable<Rubric> Rubrics { get; set; }
         public DateTime DateService { get; }
         public DateTime StartTime { get; set; }
         public DateTime EndTime { get; set; }
