7a8b0470439d60a38b19b82f84740d62f7e5e68b
RenatoPereirals <renatopreirals@gmail.com>
2024-01-12 11:42:52 -0300

    :wastebasket: Renomeia o nome de classe e interface.

--- src/Bsd.Domain/Services/Interfaces/IDayTypeRubricCalculator.cs
+++
@@ -1,9 +0,0 @@
-using Bsd.Domain.Entities;
-
-namespace Bsd.Domain.Services.Interfaces
-{
-    public interface IDayTypeRubricCalculator
-    {
-        Task<List<Rubric>> CalculateOvertimeRubricsBasedOnDayType(string employeeId);
-    }
-}
