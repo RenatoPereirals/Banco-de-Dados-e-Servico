8dbb8ac5277f529fb41bf8760f58a075253783d9
RenatoPereirals <renatopreirals@gmail.com>
2024-01-12 11:24:07 -0300

    :wrench: Ajuste na interface IHoursCalculationService. Remove um dos argumentos passados no contrato: Bsd bsd.

--- src/Bsd.Domain/Services/Interfaces/IHoursCalculationService.cs
+++ src/Bsd.Domain/Services/Interfaces/IHoursCalculationService.cs
@@ -4,6 +4,6 @@ namespace Bsd.Domain.Services.Interfaces
 {
     public interface IHoursCalculationService
     {
-        Task<List<Rubric>> CalculateOvertimeHoursList(string employeeId, Entities.Bsd bsd);
+        Task<List<Rubric>> CalculateOvertimeHoursList(string employeeId);
     }
 }
