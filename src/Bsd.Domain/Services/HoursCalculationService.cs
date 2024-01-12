c5768312d1005ef6e4daec467abd192eba82bae9
RenatoPereirals <renatopreirals@gmail.com>
2024-01-12 11:25:26 -0300

    :heavy_plus_sign: Injeção de dependências. Injeta as interfaces IDayTypeRubricCalculator e IEmployeeException.

--- src/Bsd.Domain/Services/HoursCalculationService.cs
+++ src/Bsd.Domain/Services/HoursCalculationService.cs
@@ -1,7 +1,6 @@
 using Bsd.Domain.Entities;
-using Bsd.Domain.Enums;
+using Bsd.Domain.Exceptions.Interfaces;
 using Bsd.Domain.Repository.Interfaces;
-using Bsd.Domain.Service.Interfaces;
 using Bsd.Domain.Services.Interfaces;
 
 namespace Bsd.Domain.Services
@@ -9,63 +8,26 @@ namespace Bsd.Domain.Services
     public class HoursCalculationService : IHoursCalculationService
     {
         private readonly IEmployeeRepository _employeeRepository;
-        private readonly IHoliDayChecker _holiDayChecker;
+        private readonly IDayTypeRubricCalculator _dayTypeRubricCalculator;
+        private readonly IEmployeeException _employeeException;
 
         public HoursCalculationService(IEmployeeRepository employeeRepository,
-                                          IHoliDayChecker holiDayChecker)
+                                          IDayTypeRubricCalculator dayTypeRubricCalculator,
+                                          IEmployeeException employeeException)
         {
             _employeeRepository = employeeRepository;
-            _holiDayChecker = holiDayChecker;
+            _dayTypeRubricCalculator = dayTypeRubricCalculator;
+            _employeeException = employeeException;
         }
 
-        public async Task<List<Rubric>> CalculateOvertimeHoursList(string employeeId, Entities.Bsd bsd)
+        public async Task<List<Rubric>> CalculateOvertimeHoursList(string employeeId)
         {
             var employee = await _employeeRepository.GetEmployeeByRegistrationAsync(employeeId);
-            ValidateEmployee(employee);
-
-            var date = bsd.DateService;
-            var listRubrics = await CalculateOvertimeRubricsBasedOnDayType(date, employeeId);
+            _employeeException.ValidateEmployeeIdNotNull(employee);
+            
+            var listRubrics = await _dayTypeRubricCalculator.CalculateOvertimeRubricsBasedOnDayType(employeeId);
 
             return listRubrics;
-        }
-
-        private void ValidateEmployee(Employee employee)
-        {
-            if (employee == null)
-            {
-                throw new ArgumentNullException(nameof(employee), "Funcionário não pode ser nulo");
-            }
-        }
-
-        private async Task<List<Rubric>> CalculateOvertimeRubricsBasedOnDayType(DateTime date, string employeeId)
-        {
-            var employee = await _employeeRepository.GetEmployeeByRegistrationAsync(employeeId);
-            var dayType = CalculateDayType(date);
-            var rubrics = new List<Rubric>(employee.Rubrics);
-
-            if (dayType == DayType.Sunday)
-            {
-                var sundayRubrics = new List<Rubric>
-                {
-                    new("1937", "se domingos então 7h/dia de 100%", 7.00m, DayType.Sunday, ServiceType.P140),
-                    new("1921", "se domingos ou feriados então 3h/dia de 150%", 3.00m, DayType.Sunday, ServiceType.P140)
-                };
-
-                rubrics.AddRange(sundayRubrics);
-            }
-
-            return rubrics;
-        }
-
-        private DayType CalculateDayType(DateTime date)
-        {
-            if (date.DayOfWeek == DayOfWeek.Sunday)
-                return DayType.Sunday;
-
-            if (_holiDayChecker.IsHoliday(date))
-                return DayType.HoliDay;
-
-            return DayType.Workday;
-        }
+        }        
     }
 }
