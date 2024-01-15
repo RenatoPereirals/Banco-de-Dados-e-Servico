7c9a2ee53735b85f52c492b0b57a558e74509ac1
RenatoPereirals <renatopreirals@gmail.com>
2024-01-15 12:28:41 -0300

    :sparkles: Adiciona novo método a interface. Adição do novo método GetBsdByIdAsync a interface IBsdRepository.

--- src/Bsd.Domain/Repository/Interfaces/IBsdRepository.cs
+++ src/Bsd.Domain/Repository/Interfaces/IBsdRepository.cs
@@ -1,4 +1,7 @@
 namespace Bsd.Domain.Repository.Interfaces
 {
-    public interface IBsdRepository : ICreatableUpdatable<Bsd.Domain.Entities.Bsd> { }
+    public interface IBsdRepository : ICreatableUpdatable<Bsd.Domain.Entities.Bsd> 
+    {
+        Task<Entities.Bsd> GetBsdByIdAsync(string bsdId);
+    }
 }
