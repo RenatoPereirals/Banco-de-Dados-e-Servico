2e62f6c9bcd80a5372f8071c41f68d863ba6e4cd
RenatoPereirals <renatopreirals@gmail.com>
2024-01-16 12:43:55 -0300

    :hammer: Refatora o método. Refatora o método CalculateOvertimeHoursList da classe HoursCalculationService. Diminuindo suas responsabilidades.

--- src/Bsd.Domain/Services/HoursCalculationService.cs
+++ src/Bsd.Domain/Services/HoursCalculationService.cs
@@ -14,21 +14,7 @@ namespace Bsd.Domain.Services
 
         public async Task<List<Rubric>> CalculateOvertimeHoursList(string employeeId, string bsdId)
         {
-            var dayTypeRubrics = await _rubricCalculator.CalculateOvertimeRubricsBasedOnDayType(employeeId);
-            var serviceTypeRubrics = await _rubricCalculator.CalculateOvertimeRubricsBasedOnServiceType(bsdId);
-
-            var combinedRubrics = CombineRubrics(serviceTypeRubrics, dayTypeRubrics);
-
-            return combinedRubrics;
-        }
-
-        private List<Rubric> CombineRubrics(List<Rubric> rubrics1, List<Rubric> rubrics2)
-        {
-            var rubricsSet = new HashSet<Rubric>(rubrics1); // HashSet garante unicidade
-
-            rubricsSet.UnionWith(rubrics2); // Adiciona rubricas do segundo conjunto
-
-            return rubricsSet.ToList();
+           return await _rubricCalculator.CombineRubricsList(employeeId, bsdId);             
         }
     }
 }
