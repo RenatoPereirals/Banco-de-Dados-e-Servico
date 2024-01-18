335b743131b3c40d768e9a9efea7cbe2811659e4
RenatoPereirals <renatopreirals@gmail.com>
2024-01-18 19:41:29 -0300

    :arrows_counterclockwise: | Remove referência desnecessária. Remove referências desnecessárias para simplificar o código.

--- src/Bsd.Domain/Services/Interfaces/IHoliDayChecker.cs
+++ src/Bsd.Domain/Services/Interfaces/IHoliDayChecker.cs
@@ -1,8 +1,3 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-
 namespace Bsd.Domain.Service.Interfaces
 {
     public interface IHoliDayChecker
