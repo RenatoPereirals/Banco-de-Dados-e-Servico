76bc0d2a4e7c5415dc61dd6aef7c32a7ba72109d
RenatoPereirals <renatopreirals@gmail.com>
2024-01-18 19:21:30 -0300

    :pencil: Renomeia a classe. Renomeia classe Bsd para BsdEntity, para remover possíveis conflitos com o nome do projeto.

---
+++ src/Bsd.Domain/Entities/BsdEntity.cs
@@ -0,0 +1,25 @@
+﻿using Bsd.Domain.Enums;
+namespace Bsd.Domain.Entities
+{
+public class BsdEntity
+{
+    public int BsdNumber { get; set; }
+    public List<int> EmployeeIds { get; set; }
+    public List<Employee> Employees { get; set; } 
+    public DateTime DateService { get; }
+    public DateTime StartTime { get; set; }
+    public DateTime EndTime { get; set; }
+    public DayType DayType { get; }
+    public Dictionary<int, List<Rubric>> EmployeeRubrics { get; set; } = new();
+
+    public BsdEntity(int bsdNumber,
+            List<int> employeeIds,
+            DateTime dateService)
+    {
+        BsdNumber = bsdNumber;
+        DateService = dateService;
+        EmployeeIds = employeeIds;
+    }
+}
+
+}
