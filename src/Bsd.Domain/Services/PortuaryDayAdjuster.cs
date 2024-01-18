835b131e3824d5848628a0b5666a65e6d3070c9d
RenatoPereirals <renatopreirals@gmail.com>
2024-01-18 19:43:09 -0300

    :x: | Remove Comentários Remove comentários redundantes para simplificar o código.

--- src/Bsd.Domain/Services/PortuaryDayAdjuster.cs
+++ src/Bsd.Domain/Services/PortuaryDayAdjuster.cs
@@ -1,7 +1,3 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 using Bsd.Domain.Service.Interfaces;
 
 namespace Bsd.Domain.Service
@@ -10,22 +6,17 @@ namespace Bsd.Domain.Service
     {
         public DateTime Adjust(DateTime date)
         {
-            // Verificar se o feriado é o Dia do Portuário (28/1)
             if (date.Month == 1 && date.Day == 28)
             {
-                // Verificar se o Dia do Portuário cai em um sábado ou domingo
                 if (date.DayOfWeek == DayOfWeek.Saturday)
                 {
-                    // Se for sábado, ajustar para segunda-feira
                     return date.AddDays(2);
                 }
                 else if (date.DayOfWeek == DayOfWeek.Sunday)
                 {
-                    // Se for domingo, ajustar para segunda-feira
                     return date.AddDays(1);
                 }
             }
-
             return date;
         }
     }
