e00d5a1ff368ece7edb46bf42be84403ca790329
RenatoPereirals <renatopreirals@gmail.com>
2024-01-18 19:31:48 -0300

    :x: Remove classe. Simplifica o código removendo classes desnecessárias.

--- src/Bsd.Domain/Services/Interfaces/IDayTypeAndServiceTypeRubricCalculator.cs
+++
@@ -1,9 +0,0 @@
-using Bsd.Domain.Entities;
-
-namespace Bsd.Domain.Services.Interfaces
-{
-    public interface IDayTypeAndServiceTypeRubricCalculator
-    {
-        Task<List<Rubric>> CombineRubricsList(string employeeId, string bsdId);
-    }
-}
