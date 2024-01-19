db4e9cbe8f99ab150a15f61ccd61bd687fa996f5
RenatoPereirals <renatopreirals@gmail.com>
2024-01-19 20:43:50 -0300

    ➕ | Adiciona o método GetRubricsByServiceTypeAndDayAsync     Implementa o método GetRubricsByServiceTypeAndDayAsync para retornar uma lista de rubricas com base no tipo de serviço e tipo de dia.

---
+++ src/Bsd.Domain/Services/RubricService.cs
@@ -0,0 +1,23 @@
+using Bsd.Domain.Entities;
+using Bsd.Domain.Enums;
+using Bsd.Domain.Repository.Interfaces;
+using Bsd.Domain.Services.Interfaces;
+
+namespace Bsd.Domain.Services
+{
+    public class RubricService : IRubricService
+    {
+        private readonly IRubricRepository _rubricRepository;
+
+        public RubricService(IRubricRepository rubricRepository)
+        {
+            _rubricRepository = rubricRepository;
+        }
+
+        public async Task<List<Rubric>> GetRubricsByServiceTypeAndDayAsync(ServiceType serviceType, DayType dayType)
+        {
+            var allRubrics = await _rubricRepository.GetAllRubricsAsync();
+            return allRubrics.Where(r => r.ServiceType == serviceType && r.DayType == dayType).ToList();
+        }
+    }
+}
