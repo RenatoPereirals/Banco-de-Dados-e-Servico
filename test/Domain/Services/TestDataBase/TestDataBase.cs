6c9d804a0d8367d42062af07453cb38b318e563a
RenatoPereirals <renatopreirals@gmail.com>
2024-01-20 16:36:28 -0300

    🔄 Altera a classe auxiliar e a classe TestBase     Modifica a classe auxiliar para adicionar uma lista de funcionários e renomeia a variável TestRubrics para TestRubricsList.

--- test/Domain/Services/TestDataBase/TestDataBase.cs
+++ test/Domain/Services/TestDataBase/TestDataBase.cs
@@ -5,12 +5,19 @@ namespace test.Domain.Services.TestDataBase
 {
     public class TestBase
     {
-        protected List<Rubric> TestRubrics => new List<Rubric>
+        protected static List<Rubric> TestRubricsList => new()
         {
             new("1", "a", 3.0M, DayType.HoliDay, ServiceType.P110),
             new("2", "b", 2.0M, DayType.HoliDay, ServiceType.P110),
             new("3", "c", 1.0M, DayType.Sunday, ServiceType.P140),
             new("4", "d", 1.0M, DayType.Workday, ServiceType.P140),
+        };
+
+        protected static List<Employee> TestEmployeeList => new()
+        {
+                new(1234, ServiceType.P110),
+                new(2345, ServiceType.P140),
+                new(3456, ServiceType.P140)
         };
     }
 }
