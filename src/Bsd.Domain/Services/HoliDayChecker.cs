9f651fd3f1910820ae7ed5ad1df716275ee47101
RenatoPereirals <renatopreirals@gmail.com>
2024-01-21 13:13:12 -0300

    🔄 Reorganiza a classe HoliDayChecker Reorganiza a classe HoliDayChecker movendo o construtor para o topo da classe, seguindo as convenções de estilo do C#. Isso melhora a legibilidade do código.

--- src/Bsd.Domain/Services/HoliDayChecker.cs
+++ src/Bsd.Domain/Services/HoliDayChecker.cs
@@ -4,19 +4,20 @@ namespace Bsd.Domain.Entities
 {
     public class HoliDayChecker : IHoliDayChecker
     {
-        // Lista de feriados fixos (mês, dia)
-        private static readonly List<(int, int)> FixedHolidays = new()
-        {
-            (1, 1),  (3, 6), (4, 7), (4, 21), (5, 1), (6, 24),
-            (7, 16), (9, 7), (10, 12), (11, 2), (11, 15), (12, 8), (12, 25)
-        };
-
+        
         private readonly IVariableDateHolidayAdjuster _variableDateHolidayAdjuster;
 
         public HoliDayChecker(IVariableDateHolidayAdjuster variableDateHolidayAdjuster)
         {
             _variableDateHolidayAdjuster = variableDateHolidayAdjuster;
         }
+
+        // Lista de feriados fixos (mês, dia)
+        private static readonly List<(int, int)> FixedHolidays = new()
+        {
+            (1, 1),  (3, 6), (4, 7), (4, 21), (5, 1), (6, 24),
+            (7, 16), (9, 7), (10, 12), (11, 2), (11, 15), (12, 8), (12, 25)
+        };
 
         public bool IsHoliday(DateTime date)
         {
