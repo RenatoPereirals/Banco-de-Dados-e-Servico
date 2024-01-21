fccb6c8af7ca2d13f584172100b7a728f63a66d8
RenatoPereirals <renatopreirals@gmail.com>
2024-01-21 13:03:30 -0300

    🔄 Altera a propriedade DayType para Set Muda a propriedade DayType da classe BsdEntity para uma propriedade do tipo Set.

--- src/Bsd.Domain/Entities/BsdEntity.cs
+++ src/Bsd.Domain/Entities/BsdEntity.cs
@@ -12,7 +12,7 @@ namespace Bsd.Domain.Entities
 
         public int BsdNumber { get; set; }
         public DateTime DateService { get; }
-        public DayType DayType { get; }
+        public DayType DayType { get; set; }
         public ICollection<EmployeeBsdEntity> EmployeeBsdEntities { get; set; } = new List<EmployeeBsdEntity>();
 
     }
