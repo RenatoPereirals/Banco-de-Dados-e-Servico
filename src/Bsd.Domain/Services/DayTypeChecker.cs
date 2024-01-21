9da7ed59a5d62e9445dac3057a3d5d587b56f041
RenatoPereirals <renatopreirals@gmail.com>
2024-01-21 13:09:01 -0300

    🏗️ Implementa a classe DayTypeChecker Implementa a classe DayTypeChecker com os métodos GetDayType, IsSundayAndHoliday e IsSunday. Essa classe é responsável por verificar o tipo de dia (domingo, feriado, domingo e feriado, ou dia útil) com base na data fornecida.

---
+++ src/Bsd.Domain/Services/DayTypeChecker.cs
@@ -0,0 +1,53 @@
+using Bsd.Domain.Enums;
+using Bsd.Domain.Service.Interfaces;
+using Bsd.Domain.Services.Interfaces;
+
+namespace Bsd.Domain.Services
+{
+    public class DayTypeChecker : IDayTypeChecker
+    {
+        private readonly IHoliDayChecker _holiDayChecker;
+        public DayTypeChecker(IHoliDayChecker holiDayChecker)
+        {
+            _holiDayChecker = holiDayChecker;
+
+        }
+        public DayType GetDayType(DateTime date)
+        {
+            if (IsSundayAndHoliday(date))
+            {
+                return DayType.SundayAndHoliday;
+            }
+            else if (IsSunday(date))
+            {
+                return DayType.Sunday;
+            }
+            else if (_holiDayChecker.IsHoliday(date))
+            {
+                return DayType.HoliDay;
+            }
+            else
+            {
+                return DayType.Workday;
+            }
+        }
+
+        private bool IsSundayAndHoliday(DateTime date)
+        {
+            if (date.DayOfWeek == DayOfWeek.Sunday && _holiDayChecker.IsHoliday(date))
+            {
+                return true;
+            }
+            return false;
+        }
+
+        private bool IsSunday(DateTime date)
+        {
+            if (date.DayOfWeek == DayOfWeek.Sunday)
+            {
+                return true;
+            }
+            return false;
+        }
+    }
+}
