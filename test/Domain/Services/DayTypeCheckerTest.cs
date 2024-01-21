a05416f3a5b5d2e4694d51b844d8ed7a629e4cb2
RenatoPereirals <renatopreirals@gmail.com>
2024-01-21 19:24:36 -0300

    ✅ Cria a classe de teste DayTypeCheckerTest Cria a classe de teste DayTypeCheckerTest para validar a funcionalidade do método GetDayType na classe DayTypeChecker. Os testes incluem a verificação do tipo de dia para domingos e feriados.

---
+++ test/Domain/Services/DayTypeCheckerTest.cs
@@ -0,0 +1,61 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Bsd.Domain.Enums;
+using Bsd.Domain.Service.Interfaces;
+using Bsd.Domain.Services;
+using Moq;
+using Xunit;
+
+namespace test.Domain.Services.TestDataBase
+{
+    public class DayTypeCheckerTest
+    {
+        private readonly Mock<IHoliDayChecker> _mockHolidayChecer;
+        private readonly DayTypeChecker _dayTypeChecker;
+
+        public DayTypeCheckerTest()
+        {
+            _mockHolidayChecer = new Mock<IHoliDayChecker>();
+            _dayTypeChecker = new DayTypeChecker(_mockHolidayChecer.Object);
+
+            
+        }
+
+        [Theory]
+        [InlineData("7/1/2024")]
+        [InlineData("14/1/2024")]
+        [InlineData("21/1/2024")]
+        public void Should_Return_DayType_For_Sunday_Correctly(string data)
+        {
+            // Arrange
+            var sundayDate = DateTime.Parse(data);
+            var dayTypeExpected = DayType.Sunday;
+
+            // Act
+            var dayTypeResult = _dayTypeChecker.GetDayType(sundayDate);
+
+            //Assert
+            Assert.Equal(dayTypeExpected, dayTypeResult);
+        }
+
+        [Fact]
+        public void Should_Return_DayType_For_Holiday_Correctly()
+        {
+            // Arrange
+            var HolidayDate = DateTime.Parse("1/1/2024");
+            var dayTypeExpected = DayType.HoliDay;
+
+            _mockHolidayChecer
+                .Setup(hc => hc.IsHoliday(It.IsAny<DateTime>()))
+                .Returns(true);
+
+            // Act
+            var dayTypeResult = _dayTypeChecker.GetDayType(HolidayDate);
+
+            //Assert
+            Assert.Equal(dayTypeExpected, dayTypeResult);
+        }
+    }
+}
