b2b2d974ca8e559a3cb5c3e03c53963414da4a5d
RenatoPereirals <renatopreirals@gmail.com>
2024-01-22 18:46:54 -0300

    ✨ Adiciona o método GetAllBsdAsync na interface IBsdRepository O método GetAllBsdAsync foi adicionado à interface IBsdRepository. Este método, quando implementado, retornará uma lista de todas as entidades BsdEntity de forma assíncrona.

--- src/Bsd.Domain/Repository/Interfaces/IBsdRepository.cs
+++ src/Bsd.Domain/Repository/Interfaces/IBsdRepository.cs
@@ -5,5 +5,6 @@ namespace Bsd.Domain.Repository.Interfaces
     public interface IBsdRepository : IGeralRepository<BsdEntity> 
     {
         Task<BsdEntity> GetBsdByIdAsync(int bsdId);
+        Task<IEnumerable<BsdEntity>> GetAllBsdAsync();
     }
 }
