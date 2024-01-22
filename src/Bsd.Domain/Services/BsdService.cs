d30c6fd37f90282a21bf7596d5c78c7750b8ec9e
RenatoPereirals <renatopreirals@gmail.com>
2024-01-22 18:53:08 -0300

    ✨⚙️🔨 Adiciona e refatora métodos na classe BsdService Adiciona o método CalculateEmployeeWorkedDays e injeta a interface IBsdRepository na classe BsdService. Refatora o método CreateBsdAsync para incluir um loop foreach. Essas mudanças melhoram a funcionalidade e a eficiência da classe BsdService.

--- src/Bsd.Domain/Services/BsdService.cs
+++ src/Bsd.Domain/Services/BsdService.cs
@@ -10,39 +10,43 @@ namespace Bsd.Domain.Services
         private readonly IEmployeeRepository _employeeRepository;
         private readonly IRubricService _rubricService;
         private readonly IDayTypeChecker _daytypeChecker;
+        private readonly IBsdRepository _bsdReposiory;
 
         public BsdService(IEmployeeRepository employeeRepository,
                           IRubricService rubricService,
-                          IDayTypeChecker dayTypeChecker)
+                          IDayTypeChecker dayTypeChecker,
+                          IBsdRepository bsdRepository)
         {
             _employeeRepository = employeeRepository;
             _rubricService = rubricService;
             _daytypeChecker = dayTypeChecker;
+            _bsdReposiory = bsdRepository;
         }
 
-        public async Task<BsdEntity> CreateBsdAsync(int bsdNumber, DateTime dateService, IEnumerable<int> employeeRegistrations)
+        public async Task<BsdEntity> CreateBsdAsync(int bsdNumber, DateTime dateService, List<int> employeeRegistrations)
         {
             var dayType = _daytypeChecker.GetDayType(dateService);
             var bsdEntity = new BsdEntity(bsdNumber, dateService)
             {
                 DayType = dayType
             };
-            await AddEmployeesToBsdAsync(bsdEntity, employeeRegistrations);
+
+            foreach (var employeeRegistration in employeeRegistrations)
+            {
+                await AddEmployeesToBsdAsync(bsdEntity, employeeRegistration);
+            }
             return bsdEntity;
         }
 
-        public async Task AddEmployeesToBsdAsync(BsdEntity bsdEntity, IEnumerable<int> employeeRegistrations)
+        public async Task AddEmployeesToBsdAsync(BsdEntity bsdEntity, int employeeRegistrations)
         {
-            foreach (var registration in employeeRegistrations)
-            {
-                var employee = await _employeeRepository.GetEmployeeByRegistrationAsync(registration)
-                    ?? throw new Exception("Mátricula do funcionário não encontrada.");
+            var employee = await _employeeRepository.GetEmployeeByRegistrationAsync(employeeRegistrations)
+                ?? throw new Exception("Mátricula do funcionário não encontrada.");
 
-                var employeeBsdEntity = new EmployeeBsdEntity(registration, employee, bsdEntity.BsdNumber, bsdEntity);
-                bsdEntity.EmployeeBsdEntities.Add(employeeBsdEntity);
+            var employeeBsdEntity = new EmployeeBsdEntity(employeeRegistrations, employee, bsdEntity.BsdNumber, bsdEntity);
+            bsdEntity.EmployeeBsdEntities.Add(employeeBsdEntity);
 
-                await AssignRubricsToEmployeeByServiceTypeAndDayAsync(employeeBsdEntity, bsdEntity.DayType);
-            }
+            await AssignRubricsToEmployeeByServiceTypeAndDayAsync(employeeBsdEntity, bsdEntity.DayType);
         }
 
         public async Task AssignRubricsToEmployeeByServiceTypeAndDayAsync(EmployeeBsdEntity employeeBsdEntity, DayType dayType)
@@ -50,6 +54,28 @@ namespace Bsd.Domain.Services
             var employee = employeeBsdEntity.Employee;
             var filteredRubrics = await _rubricService.FilterRubricsByServiceTypeAndDayAsync(employee.ServiceType, dayType);
             employeeBsdEntity.Rubrics = filteredRubrics;
+        }
+
+        public async Task<int> CalculateEmployeeWorkedDays(int employeeId, DateTime startDate, DateTime endDate)
+        {
+            var bsdEntities = await _bsdReposiory.GetAllBsdAsync();
+            var employee = await _employeeRepository.GetEmployeeByRegistrationAsync(employeeId);
+
+            var filterbsdEntities = bsdEntities.Where(bsdDate => bsdDate.DateService >= startDate && bsdDate.DateService <= endDate);
+
+            var workedDays = 0;
+
+            foreach (var bsdEntity in filterbsdEntities)
+            {
+                foreach (var employeeBsdEntity in bsdEntity.EmployeeBsdEntities)
+                {
+                    if (employee.Registration == employeeBsdEntity.Employee.Registration)
+                    {
+                        workedDays++;
+                    }
+                }
+            }
+            return workedDays;
         }
     }
 }
