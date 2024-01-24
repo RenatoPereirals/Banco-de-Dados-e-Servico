2741b6904a8f92c05a706ad9ecc776c7e85d1db9
RenatoPereirals <renatopreirals@gmail.com>
2024-01-24 05:03:33 -0300

    🔨 Refatora a interface IBsdRepository Remove a herança de IGeralRepository em IBsdRepository

--- src/Bsd.Domain/Repository/Interfaces/IBsdRepository.cs
+++ src/Bsd.Domain/Repository/Interfaces/IBsdRepository.cs
@@ -2,7 +2,7 @@ using Bsd.Domain.Entities;
 
 namespace Bsd.Domain.Repository.Interfaces
 {
-    public interface IBsdRepository : IGeralRepository<BsdEntity> 
+    public interface IBsdRepository
     {
         Task<BsdEntity> GetBsdByIdAsync(int bsdId);
         Task<IEnumerable<BsdEntity>> GetAllBsdAsync();
