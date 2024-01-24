b18d9a395799f6a93c83c6c7579e0fd16e274b5e
RenatoPereirals <renatopreirals@gmail.com>
2024-01-24 05:07:40 -0300

    ✨ Adiciona métodos à IGeralRepository Adiciona os métodos Delete, DeleteRange e SaveChangesAsync à interface IGeralRepository para melhorar a funcionalidade.

--- src/Bsd.Domain/Repository/Interfaces/IGeralRepository.cs
+++ src/Bsd.Domain/Repository/Interfaces/IGeralRepository.cs
@@ -1,8 +1,13 @@
+using System.Net;
+
 namespace Bsd.Domain.Repository.Interfaces
 {
-    public interface IGeralRepository<T>
+    public interface IGeralRepository
     {
-        void Create(T entity);
-        void Update(T entity);
+        void Create<T>(T entity) where T : class;
+        void Update<T>(T entity) where T : class;
+        void Delete<T>(T entity) where T : class;
+        void DeleteRange<T>(T[] entities) where T : class;
+        Task<bool> SaveChangesAsync();
     }
 }
