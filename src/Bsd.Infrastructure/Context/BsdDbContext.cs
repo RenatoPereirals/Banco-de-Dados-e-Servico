153ae2443e2b7e9df6ec3b6d3fa06b9a6799a9a0
RenatoPereirals <renatopreirals@gmail.com>
2024-01-24 05:16:51 -0300

    🏗️ Cria a classe BsdDbContext Esta classe inclui DbSets para BsdEntity, Employee, EmployeeBsdEntity e Rubric. Além disso, configura a chave primária para a entidade EmployeeBsdEntity no método OnModelCreating.

---
+++ src/Bsd.Infrastructure/Context/BsdDbContext.cs
@@ -0,0 +1,28 @@
+﻿using Bsd.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace Bsd.Infrastructure.Context
+{
+    public class BsdDbContext : DbContext
+    {
+#pragma warning disable CS8618
+
+        public BsdDbContext(DbContextOptions<BsdDbContext> options) : base(options) { }
+
+        public DbSet<BsdEntity> BsdEntities { get; set; }
+        public DbSet<Employee> Employees { get; set; }
+        public DbSet<EmployeeBsdEntity> EmployeesBsdEntities { get; set; }
+        public DbSet<Rubric> Rubrics { get; set; }
+
+#pragma warning restore CS8618
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+            modelBuilder.Entity<EmployeeBsdEntity>()
+                        .HasKey(EB => new { EB.BsdEntityNumber, EB.EmployeeRegistration });
+
+        }
+    }
+}
