7931774098b8d4fd9e9ebdd0908707379e9fea02
RenatoPereirals <renatopreirals@gmail.com>
2024-01-31 09:17:49 -0300

    🔨 Refatora a classe Employee Remove o método de cálculo do dígito verificador e a validação da matrícula. Torna o campo 'Digit' gravável através do método 'SetDigit'. Torna o campo 'Registration' gravável através do método 'SetRegistration'. Remove a inicialização padrão do campo 'ServiceType'.

--- src/Bsd.Domain/Entities/Employee.cs
+++ src/Bsd.Domain/Entities/Employee.cs
@@ -7,50 +7,24 @@ namespace Bsd.Domain.Entities
         public Employee(int registration,
                         ServiceType serviceType)
         {
-            SetRegistration(registration);
+            Registration = registration;
             ServiceType = serviceType;
         }
 
         public int Registration { get; private set; }
-        public int Digit => CalculateModulo11CheckDigit();
-        public ServiceType ServiceType { get; } = new ServiceType();
+        public int Digit { get; private set; }
+        public ServiceType ServiceType { get; }
         public DateTime DateService { get; set; }
         public ICollection<EmployeeBsdEntity> EmployeeBsdEntities { get; set; } = new List<EmployeeBsdEntity>();
 
-        private void SetRegistration(int value)
+        public void SetRegistration(int value)
         {
-            ValidateRegistration(value);
             Registration = value;
         }
 
-        private int CalculateModulo11CheckDigit()
+        public void SetDigit(int value)
         {
-            int sum = 0;
-            string registration = Registration.ToString();
-            int weight = registration.Length + 1;
-
-            foreach (char digitChar in registration)
-            {
-                if (!int.TryParse(digitChar.ToString(), out int digit))
-                {
-                    throw new FormatException($"Caractere inválido {digitChar} na mátricula");
-                }
-
-                sum += digit * weight;
-                weight--;
-            }
-
-            int mod = sum % 11;
-            return 11 - mod;
-        }
-
-        private static void ValidateRegistration(int value)
-        {
-            string registration = value.ToString();
-            if (registration.Length != 4)
-            {
-                throw new ArgumentException("A matrícula deve conter 4 dígitos");
-            }
+            Digit = value;
         }
     }
 }
