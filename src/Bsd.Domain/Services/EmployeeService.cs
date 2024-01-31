d178f02da24958347c702065987a2eb19fdce846
RenatoPereirals <renatopreirals@gmail.com>
2024-01-31 09:19:07 -0300

    ✨ Cria a classe EmployeeService Adiciona a classe EmployeeService que contém os métodos de validação e cálculo do dígito verificador que foram removidos da classe Employee. Agora, a classe EmployeeService é responsável por definir a matrícula e o dígito verificador do Employee.

--- src/Bsd.Domain/Services/EmployeeService.cs
+++ src/Bsd.Domain/Services/EmployeeService.cs
@@ -1,7 +1,52 @@
+using Bsd.Domain.Entities;
+
 namespace Bsd.Domain.Services
 {
     public class EmployeeService
     {
-        
+        private readonly Employee _employee;
+        public EmployeeService(Employee employee)
+        {
+            _employee = employee;
+        }
+
+        public void SetRegistrationAndDigit(int value)
+        {
+            ValidateRegistration(value);
+            _employee.SetRegistration(value);
+            _employee.SetDigit(CalculateModulo11CheckDigit(value));
+        }
+
+        private static void ValidateRegistration(int value)
+        {
+            string registration = value.ToString();
+            if (registration.Length != 4)
+            {
+                throw new ArgumentException($"A matrícula {value}, deve conter 4 dígitos");
+            }
+        }
+
+        private static int CalculateModulo11CheckDigit(int value)
+        {
+            int sum = 0;
+            string registration = value.ToString();
+            int weight = registration.Length + 1;
+
+            foreach (char digitChar in registration)
+            {
+                if (!int.TryParse(digitChar.ToString(), out int digit))
+                {
+                    throw new FormatException($"Caractere inválido {digitChar} na mátricula");
+                }
+
+                sum += digit * weight;
+                weight--;
+            }
+
+            int mod = sum % 11;
+            return 11 - mod;
+        }
+
+
     }
 }
