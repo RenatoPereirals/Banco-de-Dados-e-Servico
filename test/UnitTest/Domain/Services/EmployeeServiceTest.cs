10fa555d7d74120030188d38e5c88fd9bc5b3662
RenatoPereirals <renatopreirals@gmail.com>
2024-01-31 10:38:40 -0300

    ✅ Criação da classe de teste EmployeeServiceTest

---
+++ test/UnitTest/Domain/Services/EmployeeServiceTest.cs
@@ -0,0 +1,17 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace test.UnitTest.Domain.Services
+{
+    public class EmployeeServiceTest
+    {
+        [Fact]
+        public void Test1()
+        {
+            Assert.True(true);
+        }
+    }
+}
