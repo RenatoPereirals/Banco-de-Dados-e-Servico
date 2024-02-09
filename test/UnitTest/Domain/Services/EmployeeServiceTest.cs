d2ebbafd9a070f03beb454f2d2f8579e09855f2f
RenatoPereirals <renatopreirals@gmail.com>
2024-02-09 14:08:27 -0300

    🔨 Refatora a classe EmployeeServiceTest Este commit faz várias alterações na classe EmployeeServiceTest: - 🗑️ Remove o método ValidateRegistration da interface IEmployeeService. Este método agora é privado e não faz mais parte da interface pública da classe EmployeeService. - ✨ Adiciona o método CalculateModulo11CheckDigit para validar o dígito de verificação do registro. - ✅ Adiciona testes para verificar o cálculo correto do dígito de verificação. - ✅ Adiciona testes para lidar com registros inválidos e caracteres inválidos. - 🔄 Modifica os testes existentes para usar uma instância de EmployeeService compartilhada.

--- test/UnitTest/Domain/Services/EmployeeServiceTest.cs
+++ test/UnitTest/Domain/Services/EmployeeServiceTest.cs
@@ -7,35 +7,59 @@ namespace test.UnitTest.Domain.Services
     public class EmployeeServiceTest
     {
         private readonly Mock<IBsdRepository> _mockBsdRpository;
+        private readonly EmployeeService _employeeService;
 
         public EmployeeServiceTest()
         {
             _mockBsdRpository = new Mock<IBsdRepository>();
+            _employeeService = new EmployeeService(_mockBsdRpository.Object);
         }
 
         [Fact]
-        public void Should_Validation_Registration_True()
+        public void Should_CalculateModulo11CheckDigit_For_ValidRegistration()
         {
             // Arrange
             var registration = 3782;
-            var employeeService = new EmployeeService(_mockBsdRpository.Object);
 
            // Act
-            employeeService.ValidateRegistration(registration);
+            _employeeService.CalculateModulo11CheckDigit(registration);
 
             // Assert
             // Nada a fazer, se o método não lançar uma exceção, o teste passa
         }
 
         [Fact]
-        public void Should_Validation_Registration_False()
+        public void Should_Return_Exception_For_InvalidRegistration()
         {
             // Arrange
-            var registration = 123;
-            var employeeService = new EmployeeService(_mockBsdRpository.Object);
+            var invalidRegistration  = 123;
 
             // Act and Assert
-            Assert.Throws<ArgumentException>(() => employeeService.ValidateRegistration(registration));
+            Assert.Throws<ArgumentException>(() => _employeeService.CalculateModulo11CheckDigit(invalidRegistration ));
+        }
+
+        [Fact]
+        public void Should_Return_Exception_For_InvalidCharacter()
+        {
+            // Arrange
+            var invalidRegistration  = -123;
+
+            // Act and Assert
+            Assert.Throws<ArgumentException>(() => _employeeService.CalculateModulo11CheckDigit(invalidRegistration ));
+        }
+
+        [Fact]
+        public void Should_Return_Modulo11CheckDigit_For_Valid_Registration_Correctly()
+        {
+            // Arrange
+            var registration = 3782;
+            var expectedResul = 6;
+
+            // Act
+            var result =_employeeService.CalculateModulo11CheckDigit(registration);
+
+            // Assert
+            Assert.Equal(expectedResul, result);
         }
     }
 }
