86311e1915cecf9432229fb39db3b18430f1ae4b
RenatoPereirals <renatopreirals@gmail.com>
2024-02-09 14:05:17 -0300

    🗑️ Remove o método ValidateRegistration da interface IEmployeeService Este método agora é privado e não faz mais parte da interface pública da classe EmployeeService.

--- src/Bsd.Domain/Services/Interfaces/IEmployeeService.cs
+++ src/Bsd.Domain/Services/Interfaces/IEmployeeService.cs
@@ -3,7 +3,6 @@ namespace Bsd.Domain.Services.Interfaces
     public interface IEmployeeService
     {
         Task<int> CalculateEmployeeWorkedDays(int employeeRegistration, DateTime startDate, DateTime endDate);
-        void ValidateRegistration(int registration);
         int CalculateModulo11CheckDigit(int registration);
     }
 }
