c8a12a384dae30cbd101841c982ff966271bec3c
RenatoPereirals <renatopreirals@gmail.com>
2024-02-14 16:49:41 -0300

    ✅ Adiciona o teste Should_Calculate_Employee_Worked_Days_Correctly Cria o novo teste na classe EmployeeServiceTest para verificar se o método CalculateEmployeeWorkedDays retorna a quantidade de dias trabalhados corretamente.

--- test/UnitTest/Domain/Services/EmployeeServiceTest.cs
+++ test/UnitTest/Domain/Services/EmployeeServiceTest.cs
@@ -1,10 +1,11 @@
 using Bsd.Domain.Repository.Interfaces;
 using Bsd.Domain.Services;
 using Moq;
+using test.Domain.Services.TestDataBase;
 
 namespace test.UnitTest.Domain.Services
 {
-    public class EmployeeServiceTest
+    public class EmployeeServiceTest : TestBase
     {
         private readonly Mock<IBsdRepository> _mockBsdRpository;
         private readonly EmployeeService _employeeService;
@@ -53,13 +54,34 @@ namespace test.UnitTest.Domain.Services
         {
             // Arrange
             var registration = 3782;
-            var expectedResul = 6;
 
             // Act
             var result =_employeeService.CalculateModulo11CheckDigit(registration);
 
             // Assert
-            Assert.Equal(expectedResul, result);
+            var expectedDigit = 6;
+            Assert.Equal(expectedDigit, result);
         }
+
+        [Fact]
+        public async Task Should_Calculate_Employee_Worked_Days_Correctly()
+        {
+            // Arrange
+            var employeeRegistration = 1234;
+            var startDate = new DateTime(2024, 1, 1);
+            var endDate = new DateTime(2024, 12, 31);
+
+            var testBsdEntitiesList = TestBsdList;
+
+            _mockBsdRpository.Setup(r => r.GetAllBsdAsync()).ReturnsAsync(testBsdEntitiesList);
+
+            // Act
+            var workedDays = await _employeeService.CalculateEmployeeWorkedDays(employeeRegistration, startDate, endDate);
+
+            // Assert
+            var expectedWorkedDays = daysWorked; 
+            Assert.Equal(expectedWorkedDays, workedDays);
+        }
+
     }
 }
