fde22950d6b1988253cf614b7c914cdf67e04117
RenatoPereirals <renatopreirals@gmail.com>
2024-02-26 09:13:10 -0300

    ✨ Adiciona nova funcionalidade Adiciona o método AddEmployeesToBsdAsync a interface IBsdService.

--- src/Bsd.Domain/Services/Interfaces/IBsdService.cs
+++ src/Bsd.Domain/Services/Interfaces/IBsdService.cs
@@ -5,6 +5,7 @@ namespace Bsd.Domain.Services.Interfaces
 {
     public interface IBsdService
     {
-        Task<BsdEntity> CreateBsdAsync(int bsdNumber, DateTime dateService, List<int> employeeRegistrations);
+        Task<BsdEntity> CreateBsdAsync(int bsdNumber, DateTime dateService, int employeeRegistrations, int digit);
+        Task AddEmployeesToBsdAsync(BsdEntity bsdEntity, int employeeRegistrations, int digit);
     }
 }
