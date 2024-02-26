4b9ed55967a1ac7a9a52297c289998e48a17705a
RenatoPereirals <renatopreirals@gmail.com>
2024-02-26 09:15:33 -0300

    ➕ Adiciona a classe EmployeeBsdEntityDto Cria a classe EmployeeBsdEntityDto no namespace Bsd.Application.DTOs.

--- src/Bsd.Application/DTOs/BsdEntityDto.cs
+++ src/Bsd.Application/DTOs/BsdEntityDto.cs
@@ -1,12 +1,12 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
 
 namespace Bsd.Application.DTOs
 {
-    public class BsdEntityDto
+    public class EmployeeBsdEntityDto
     {
-        
+        [Required(ErrorMessage = "O campo {0} é obrigatório.")]
+        [RegularExpression(@"^\d{4}$", ErrorMessage = "O campo {0} deve conter 4 dígitos.")]
+        public int Registration { get; set; }
+        public int Digit { get; set; }
     }
 }
