ebdf8076430927caa9742a32be9023e1b7bf9636
RenatoPereirals <renatopreirals@gmail.com>
2024-02-29 08:52:15 -0300

    🔨 Refatora a classe BsdEntity Remove o contrutor da classe BsdEntity, pois esta gerando erro na hora de gerar o banco de dados.

--- src/Bsd.Domain/Entities/BsdEntity.cs
+++ src/Bsd.Domain/Entities/BsdEntity.cs
@@ -3,17 +3,9 @@ namespace Bsd.Domain.Entities
 {
     public class BsdEntity
     {
-        public BsdEntity(int bsdNumber,
-                         DateTime dateService)
-        {
-            BsdNumber = bsdNumber;
-            DateService = dateService;
-        }
-
         public int BsdNumber { get; set; }
         public DateTime DateService { get; }
         public DayType DayType { get; set; }
         public ICollection<EmployeeBsdEntity> EmployeeBsdEntities { get; set; } = new List<EmployeeBsdEntity>();
-
     }
 }
