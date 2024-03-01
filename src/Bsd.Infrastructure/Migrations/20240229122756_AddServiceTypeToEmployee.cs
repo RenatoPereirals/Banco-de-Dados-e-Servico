a3ba4ee4ae62239647361a381e50ef22d73c961f
RenatoPereirals <renatopreirals@gmail.com>
2024-03-01 08:48:51 -0300

    ✨ Cira novas Migrations para o porjeto Cria a migration AddServiceTypeToEmployee através do comando dotnet ef migrations add InitialMigrations

---
+++ src/Bsd.Infrastructure/Migrations/20240229122756_AddServiceTypeToEmployee.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Bsd.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddServiceTypeToEmployee : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "ServiceType",
+                table: "Employees",
+                type: "INTEGER",
+                nullable: false,
+                defaultValue: 0);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "ServiceType",
+                table: "Employees");
+        }
+    }
+}
