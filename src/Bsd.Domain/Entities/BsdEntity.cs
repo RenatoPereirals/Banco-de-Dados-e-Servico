c5e4bee394cb5970ae86e0946eefe3a035c2c115
RenatoPereirals <renatopreirals@gmail.com>
2024-03-01 09:14:43 -0300

    ✨ Adiciona o método Set a propriedade DateService

--- src/Bsd.Domain/Entities/BsdEntity.cs
+++ src/Bsd.Domain/Entities/BsdEntity.cs
@@ -4,7 +4,7 @@ namespace Bsd.Domain.Entities
     public class BsdEntity
     {
         public int BsdNumber { get; set; }
-        public DateTime DateService { get; }
+        public DateTime DateService { get; set; }
         public DayType DayType { get; set; }
         public ICollection<EmployeeBsdEntity> EmployeeBsdEntities { get; set; } = new List<EmployeeBsdEntity>();
     }
