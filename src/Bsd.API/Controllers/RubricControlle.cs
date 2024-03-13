425596c91e2b480119ce454f112eb17e6b932329
RenatoPereirals <renatopreirals@gmail.com>
2024-03-13 09:00:35 -0300

    ✨ Adiciona RubricController Cria a classe RubricController para implementar os endpoints necessários para as operações de CRUD.

---
+++ src/Bsd.API/Controllers/RubricControlle.cs
@@ -0,0 +1,31 @@
+using Bsd.Application.Interfaces;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Bsd.API.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class RubricControlle : ControllerBase
+    {
+        private readonly IRubricApplicationService _rubricApplication;
+
+        public RubricControlle(IRubricApplicationService rubricApplication)
+        {
+            _rubricApplication = rubricApplication;
+        }
+
+        [HttpPost("create")]
+        public async Task<IActionResult> CreateRubric(string code, string description, decimal hoursPerDay, string dayType, string serviceType)
+        {
+            try
+            {
+                await _rubricApplication.CreateRubricAsync(code, description, hoursPerDay, dayType, serviceType);
+                return Ok();
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(ex.Message);
+            }
+        }
+    }
+}
