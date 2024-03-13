4086fc4481f4e65d4a421c6e87cb03976d894b11
RenatoPereirals <renatopreirals@gmail.com>
2024-03-13 10:15:06 -0300

    🔄 Altera o tipo de retorno do contrato na interface IRubricService Altera o tipo de retorno do contrato FilterRubricsByServiceTypeAndDayAsync de List para IEnumerable

--- src/Bsd.Domain/Services/Interfaces/IRubricService.cs
+++ src/Bsd.Domain/Services/Interfaces/IRubricService.cs
@@ -5,7 +5,7 @@ namespace Bsd.Domain.Services.Interfaces
 {
     public interface IRubricService
     {
-        Task<List<Rubric>> FilterRubricsByServiceTypeAndDayAsync(ServiceType serviceType, DayType dayType);
+        Task<IEnumerable<Rubric>> FilterRubricsByServiceTypeAndDayAsync(ServiceType serviceType, DayType dayType);
         Task<List<EmployeeRubricHours>> CalculateTotalHoursPerMonthByRubricsForEmployeeAsync(int registration, DateTime startDate, DateTime endDate);
     }
 }
