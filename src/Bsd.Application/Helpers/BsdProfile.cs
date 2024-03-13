c8716ac436e6b86668fc4caf5b83ed806b5e3052
RenatoPereirals <renatopreirals@gmail.com>
2024-03-13 09:20:59 -0300

    ✨ Adiciona a classe BsdProfile Cria a classe BsdProfile para configurar o mapeamente entre as classes de entidade e as classe DTO

---
+++ src/Bsd.Application/Helpers/BsdProfile.cs
@@ -0,0 +1,15 @@
+using System.Security.Cryptography.X509Certificates;
+using AutoMapper;
+using Bsd.Application.DTOs;
+using Bsd.Domain.Entities;
+
+namespace Bsd.Application.Helpers
+{
+    public class BsdProfile : Profile
+    {
+        public BsdProfile()
+        {
+            CreateMap<BsdEntity, BsdEntityDto>().ReverseMap();
+        }
+    }
+}
