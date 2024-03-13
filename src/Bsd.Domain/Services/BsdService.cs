7913a5ff6f1edc0ef305ccb3d496e569eb870d66
RenatoPereirals <renatopreirals@gmail.com>
2024-03-13 10:11:31 -0300

    🚚 Move o método CreateBsdAsync da classe BsdService Move o método CreateBsdAsync para a classe BsdRepository Injeta a interface IBsdRepositiry no construtor da classe BsdService

--- src/Bsd.Domain/Services/BsdService.cs
+++ src/Bsd.Domain/Services/BsdService.cs
@@ -1,5 +1,3 @@
-using Bsd.Domain.Entities;
-using Bsd.Domain.Enums;
 using Bsd.Domain.Repository.Interfaces;
 using Bsd.Domain.Services.Interfaces;
 
@@ -7,86 +5,19 @@ namespace Bsd.Domain.Services
 {
     public class BsdService : IBsdService
     {
-        private readonly IEmployeeRepository _employeeRepository;
-        private readonly IRubricService _rubricService;
-        private readonly IDayTypeChecker _daytypeChecker;
-        private readonly IEmployeeService _employeeService;
         private readonly IGeralRepository _geralRepository;
+        private readonly IBsdRepository _bsdRepository;
 
-        public BsdService(IEmployeeRepository employeeRepository,
-                          IRubricService rubricService,
-                          IDayTypeChecker dayTypeChecker,
-                          IEmployeeService employeeService,
-                          IGeralRepository geralRepository)
+        public BsdService(IGeralRepository geralRepository,
+                          IBsdRepository bsdRepository)
         {
-            _employeeRepository = employeeRepository;
-            _rubricService = rubricService;
-            _daytypeChecker = dayTypeChecker;
-            _employeeService = employeeService;
             _geralRepository = geralRepository;
+            _bsdRepository = bsdRepository;
         }
 
-        public async Task<BsdEntity> CreateBsdAsync(int bsdNumber, DateTime dateService, int employeeRegistration, int digit)
+        public async Task CreateBsdAsync(int bsdNumber, DateTime dateService, int employeeRegistration, int digit)
         {
-            try
-            {
-                _ = await _employeeRepository.GetEmployeeByRegistrationAsync(employeeRegistration)
-                    ?? throw new ArgumentException("Matrícula do funcionário não encontrada.");
-
-                var currentDigit = _employeeService.CalculateModulo11CheckDigit(employeeRegistration);
-
-                if (currentDigit != digit)
-                    throw new Exception("O dígito está incorreto.");
-
-                var dayType = _daytypeChecker.GetDayType(dateService);
-                
-                var bsdEntity = new BsdEntity()
-                {
-                    BsdNumber = bsdNumber,
-                    DateService = dateService,
-                    DayType = dayType,
-                };
-
-                _geralRepository.Create(bsdEntity);
-                await _geralRepository.SaveChangesAsync();
-
-                return bsdEntity;
-            }
-            catch (Exception ex)
-            {
-                Console.WriteLine($"Error creating BSD: {ex}");
-                throw;
-            }
-        }
-
-
-        public async Task AddEmployeesToBsdAsync(BsdEntity bsdEntity, int employeeRegistration, int digit)
-        {
-            var employee = await _employeeRepository.GetEmployeeByRegistrationAsync(employeeRegistration)
-                ?? throw new Exception("Mátricula do funcionário não encontrada.");
-
-            var currentDigit = _employeeService.CalculateModulo11CheckDigit(employeeRegistration);
-
-            if (currentDigit != digit)
-                throw new Exception("O digito está incorreto.");
-
-            var employeeBsdEntity = new EmployeeBsdEntity()
-            {
-                EmployeeRegistration = employeeRegistration,
-                Employee = employee,
-                BsdEntityNumber = bsdEntity.BsdNumber,
-                BsdEntity = bsdEntity
-            };
-            bsdEntity.EmployeeBsdEntities.Add(employeeBsdEntity);
-
-            await AssignRubricsToEmployeeByServiceTypeAndDayAsync(employeeBsdEntity, bsdEntity.DayType);
-        }
-
-        public async Task AssignRubricsToEmployeeByServiceTypeAndDayAsync(EmployeeBsdEntity employeeBsdEntity, DayType dayType)
-        {
-            var employee = employeeBsdEntity.Employee;
-            var filteredRubrics = await _rubricService.FilterRubricsByServiceTypeAndDayAsync(employee.ServiceType, dayType);
-            employeeBsdEntity.Rubrics = filteredRubrics;
+            await _bsdRepository.CreateBsdAsync(bsdNumber, dateService, employeeRegistration, digit);
         }
     }
 }
