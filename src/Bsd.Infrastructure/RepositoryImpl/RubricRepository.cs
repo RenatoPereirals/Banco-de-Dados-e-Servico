04382c493ccadefc8cd98b21171b6170584b0292
RenatoPereirals <renatopreirals@gmail.com>
2024-03-13 11:17:56 -0300

    Implementa o contrato CreateRubricAsync Implementa o contrato CreateRubricAsync da interface IRubricRepository na classe RubricRepository

--- src/Bsd.Infrastructure/RepositoryImpl/RubricRepository.cs
+++ src/Bsd.Infrastructure/RepositoryImpl/RubricRepository.cs
@@ -1,4 +1,5 @@
 using Bsd.Domain.Entities;
+using Bsd.Domain.Enums;
 using Bsd.Domain.Repository.Interfaces;
 using Bsd.Infrastructure.Context;
 using Bsd.Infrastructure.RepositoryImpl;
@@ -9,13 +10,31 @@ namespace Bsd.Domain.Persistence.RepositoryImpl
     public class RubricRepository : GeralRepository, IRubricRepository
     {
         private readonly BsdDbContext _context;
-        public RubricRepository(BsdDbContext context) : base(context)
+        private readonly IGeralRepository _geralRepository;
+        public RubricRepository(BsdDbContext context,
+                                IGeralRepository geralRepository) : base(context)
         {
             _context = context;
+            _geralRepository = geralRepository;
         }
         public async Task<IEnumerable<Rubric>> GetAllRubricsAsync()
         {
             return await _context.Rubrics.ToListAsync();
+        }
+
+        public async Task CreateRubricAsync(string code, string description, decimal hoursPerDay, DayType dayType, ServiceType serviceType)
+        {
+            var rubric = new Rubric
+            {
+                Code = code,
+                Description = description,
+                HoursPerDay = hoursPerDay,
+                DayType = dayType,
+                ServiceType = serviceType
+            };
+
+            _geralRepository.Create(rubric);
+            await _geralRepository.SaveChangesAsync();
         }
     }
 }
