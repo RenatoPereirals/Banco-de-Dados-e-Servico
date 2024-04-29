0e4168a53ea87806db116b27b73fc880c654f432
Renato Pereira <134110833+RenatoPereirals@users.noreply.github.com>
2024-04-29 09:15:50 -0300

    🔨 Refatora a classe BsdEntity Refatora a classe BsdEntity para atender aos padrões de encapsulamento

--- src/Bsd.Domain/Entities/BsdEntity.cs
+++ src/Bsd.Domain/Entities/BsdEntity.cs
@@ -3,9 +3,34 @@ namespace Bsd.Domain.Entities
 {
     public class BsdEntity
     {
-        public int BsdNumber { get; set; }
-        public DateTime DateService { get; set; }
-        public DayType DayType { get; set; }
-        public IEnumerable<EmployeeBsdEntity> EmployeeBsdEntities { get; set; } = new List<EmployeeBsdEntity>();
+        private int _bsdNumber;
+        private DateTime _dateService;
+        private DayType _dayTape;
+        private IEnumerable<EmployeeBsdEntity> _employeeBsdEntities = new List<EmployeeBsdEntity>();
+
+        public int BsdNumber
+        {
+            get { return _bsdNumber; }
+            set { _bsdNumber = value; }
+        }
+
+        public DateTime DateService
+        {
+            get { return _dateService; }
+            set { _dateService = value; }
+        }
+
+        public DayType DayType
+        {
+            get { return _dayTape; }
+            set { _dayTape = value; }
+        }
+
+        public IEnumerable<EmployeeBsdEntity> EmployeeBsdEntities
+        {
+            get { return _employeeBsdEntities; }
+            set { _employeeBsdEntities = value; }
+        }
+
     }
 }
