577021645558ead8e2f7770870398e203639fde7
renatoPreirals <renatopreirals@gmail.com>
2024-06-05 14:47:16 -0300

    refactor: Update EmployeeTest to reflect changes in Employee class
    - Adjusted EmployeeTest to reflect changes in the Employee class, replacing references to EmployeeBsdEntities with BsdEntities.
    - Updated assertions accordingly to ensure proper initialization of properties.

--- test/UnitTest/Domain/Entities/EmployeeTest.cs
+++ test/UnitTest/Domain/Entities/EmployeeTest.cs
@@ -12,17 +12,17 @@ namespace test.Domain.Entities
             var employee = new Employee();
             var expectedregistration = 1234;
             var expectedService = ServiceType.P140;
-            var expectedEmployeeList = new List<EmployeeBsdEntity>();
+            var expectedEmployeeList = new List<BsdEntity>();
 
             // Act
             employee.Registration = expectedregistration;
             employee.ServiceType = expectedService;
-            employee.EmployeeBsdEntities = expectedEmployeeList;
+            employee.BsdEntities = expectedEmployeeList;
 
             // Assert
             Assert.Equal(expectedregistration, employee.Registration);
             Assert.Equal(expectedService, employee.ServiceType);
-            Assert.Same(expectedEmployeeList, employee.EmployeeBsdEntities);
+            Assert.Same(expectedEmployeeList, employee.BsdEntities);
         }
     }
 }
