ff9488655371fc60ee197118e8e4bb412a667429
renatoPreirals <renatopreirals@gmail.com>
2024-06-05 16:44:41 -0300

    refactor: Update Employee class properties and annotations
    - Replaced EmployeeBsdEntities with BsdEntities to maintain consistency with the naming convention.
    - Changed the type of BsdEntities to ICollection<BsdEntity> for clarity and consistency.
    - Updated the constructor to initialize the BsdEntities collection.
    - Added annotations to Employee properties for improved data handling and validation.

--- src/Bsd.Domain/Entities/Employee.cs
+++ src/Bsd.Domain/Entities/Employee.cs
@@ -1,28 +1,20 @@
+using System.ComponentModel.DataAnnotations;
 using Bsd.Domain.Enums;
 
 namespace Bsd.Domain.Entities
 {
     public class Employee
     {
+        public Employee()
+        {
+            BsdEntities = new HashSet<BsdEntity>();
+        }
+
+        [Key]
         public int Registration { get; set; }
         public int Digit { get; set; }
         public ServiceType ServiceType { get; set; }
         public DateTime DateService { get; set; }
-        public ICollection<EmployeeBsdEntity> EmployeeBsdEntities { get; set; } = new List<EmployeeBsdEntity>();
-
-        public void SetRegistration(int value)
-        {
-            Registration = value;
-        }
-
-        public void SetDigit(int value)
-        {
-            Digit = value;
-        }
-
-        public void SetServiceType(ServiceType service)
-        {
-            ServiceType = service;
-        }
+        public ICollection<BsdEntity> BsdEntities { get; set; }
     }
 }
