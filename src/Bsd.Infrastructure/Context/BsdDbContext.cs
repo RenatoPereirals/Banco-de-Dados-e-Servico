735ea7044e6bdcb3542caa59cf7b45eace38ea84
renatoPreirals <renatopreirals@gmail.com>
2024-06-06 14:16:39 -0300

    refactor: Simplify and streamline BsdDbContext class
    - Removed unnecessary `DbSet` properties for `EmployeeBsdEntity` and `Rubric` to simplify the DbContext.
    - Retained essential `DbSet` properties for `BsdEntity` and `Employee`.
    - Cleaned up `OnModelCreating` method to reflect the removal of unnecessary entity configurations.
    - Ensured configuration warnings and logging are set up properly in `OnConfiguring`.
    - Updated namespaces and directives for consistency and clarity.

--- src/Bsd.Infrastructure/Context/BsdDbContext.cs
+++ src/Bsd.Infrastructure/Context/BsdDbContext.cs
@@ -1,9 +1,5 @@
 ﻿using Bsd.Domain.Entities;
 using Microsoft.EntityFrameworkCore;
-using Microsoft.EntityFrameworkCore.Diagnostics;
-using Microsoft.Extensions.Logging;
-using Newtonsoft.Json;
-
 
 namespace Bsd.Infrastructure.Context
 {
@@ -15,34 +11,7 @@ namespace Bsd.Infrastructure.Context
 
         public DbSet<BsdEntity> BsdEntities { get; set; }
         public DbSet<Employee> Employees { get; set; }
-        public DbSet<EmployeeBsdEntity> EmployeesBsdEntities { get; set; }
-        public DbSet<Rubric> Rubrics { get; set; }
 
 #pragma warning restore CS8618
-        protected override void OnModelCreating(ModelBuilder modelBuilder)
-        {
-            base.OnModelCreating(modelBuilder);
-
-            modelBuilder.Entity<EmployeeBsdEntity>()
-                        .HasKey(eb => new { eb.EmployeeRegistration, eb.BsdNumber });
-
-            modelBuilder.Entity<BsdEntity>()
-                        .HasKey(b => b.BsdNumber);
-
-            modelBuilder.Entity<Employee>()
-                        .HasKey(e => e.Registration);
-
-            modelBuilder.Entity<Rubric>()
-                        .HasKey(r => r.Code);
-        }
-
-        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
-        {
-            if (!optionsBuilder.IsConfigured)
-            {
-                optionsBuilder.ConfigureWarnings(warnings => warnings.Throw(RelationalEventId.MultipleCollectionIncludeWarning));
-            }
-            optionsBuilder.LogTo(Console.WriteLine, LogLevel.Information);
-        }
     }
 }
