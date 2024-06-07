52eac4e316155424ff663650f8f0635a2fb453eb
renatoPreirals <renatopreirals@gmail.com>
2024-06-07 15:10:34 -0300

    refactor: Enhance DateHelper to handle null, empty, and invalid date formats
    - Added null or empty check for the date string with ArgumentNullException.
    - Implemented TryParseExact to handle and validate date format.
    - Included exception handling to provide more descriptive error messages for invalid date formats.
    - Added logging comments for potential future use.

--- src/Bsd.Application/Helpers/DateHelper.cs
+++ src/Bsd.Application/Helpers/DateHelper.cs
@@ -6,8 +6,30 @@ namespace Bsd.API.Helpers
     {
         public static DateTime ParseDate(string date)
         {
-            string dateDecoded = System.Net.WebUtility.UrlDecode(date);
-            return  DateTime.ParseExact(dateDecoded, "dd/MM/yyyy", CultureInfo.InvariantCulture);
-        } 
+            if (string.IsNullOrEmpty(date))
+            {
+                throw new ArgumentNullException(nameof(date), "o campo data é obrigatória");
+            }
+
+            try
+            {
+                string dateDecoded = System.Net.WebUtility.UrlDecode(date);
+                string[] formats = { "dd/MM/yyyy" };
+                if (DateTime.TryParseExact(dateDecoded, formats, CultureInfo.InvariantCulture, DateTimeStyles.None, out DateTime parsedDate))
+                {
+                    return parsedDate;
+                }
+                else
+                {
+                    // _logger.LogError(ex, "Invalid date format");
+                    throw new FormatException("Invalid date format");
+                }
+            }
+            catch (Exception)
+            {
+                // _logger.LogError(ex, "Failed to parse date");
+                throw new FormatException("Failed to parse date");
+            }
+        }
     }
 }
