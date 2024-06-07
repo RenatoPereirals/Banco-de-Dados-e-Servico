7694b321750f2aefdf0c6f9a42423399aceccfeb
renatoPreirals <renatopreirals@gmail.com>
2024-06-07 15:21:36 -0300

    test: Refactor unit tests for BsdController
    - Added unit tests for the BsdController class to ensure correct behavior of the Post method.
    - Implemented tests for scenarios where employee is not found, and an exception is thrown.
    - Mocked dependencies such as IBsdApplicationService, IEmployeeValidationService, and repositories for isolated testing.
    - Ensured comprehensive coverage of different outcomes and error handling scenarios.

--- test/UnitTest/API/BsdControllerTest.cs
+++ test/UnitTest/API/BsdControllerTest.cs
@@ -1,104 +1,114 @@
-using Bsd.API.Controllers;
-using Bsd.Application.DTOs;
-using Bsd.Application.Interfaces;
-using Bsd.Domain.Entities;
-using Bsd.Domain.Repository.Interfaces;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Moq;
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using Xunit;
 
-namespace test.API;
-public class BsdControllerTests
+using Bsd.API.Controllers;
+using Bsd.Application.DTOs;
+using Bsd.Application.Helpers.Interfaces;
+using Bsd.Application.Interfaces;
+using Bsd.Domain.Entities;
+using Bsd.Domain.Repository.Interfaces;
+
+namespace test.API
 {
-    private readonly Mock<IBsdRepository> _mockBsdRepository;
-    private readonly Mock<IEmployeeRepository> _mockEmployeeRepository;
-    private readonly Mock<IBsdApplicationService> _mockBsdApplication;
-    private readonly BsdController _bsdController;
-    private readonly CreateBsdRequest _request;
-
-    public BsdControllerTests()
+    public class BsdControllerTests
     {
-        _mockBsdRepository = new Mock<IBsdRepository>();
-        _mockEmployeeRepository = new Mock<IEmployeeRepository>();
-        _mockBsdApplication = new Mock<IBsdApplicationService>();
+        private readonly Mock<IBsdRepository> _mockBsdRepository;
+        private readonly Mock<IEmployeeRepository> _mockEmployeeRepository;
+        private readonly Mock<IBsdApplicationService> _mockBsdApplication;
+        private readonly Mock<IEmployeeValidationService> _mockEmployeeValidationService;
+        private readonly BsdController _bsdController;
+        private readonly CreateBsdRequest _request;
 
-        _bsdController = new BsdController(_mockBsdRepository.Object,
-                                           _mockBsdApplication.Object,
-                                           _mockEmployeeRepository.Object);
-
-        _request = new CreateBsdRequest
+        public BsdControllerTests()
         {
-            BsdNumber = 1234,
-            EmployeeRegistration = 1235,
-            DateService = DateTime.UtcNow.ToString(),
-            Digit = 8
-        };
-    }
+            _mockBsdRepository = new Mock<IBsdRepository>();
+            _mockEmployeeRepository = new Mock<IEmployeeRepository>();
+            _mockBsdApplication = new Mock<IBsdApplicationService>();
+            _mockEmployeeValidationService = new Mock<IEmployeeValidationService>();
 
-    public override bool Equals(object? obj)
-    {
-        return base.Equals(obj);
-    }
+            _bsdController = new BsdController(_mockBsdRepository.Object,
+                                               _mockBsdApplication.Object,
+                                               _mockEmployeeRepository.Object,
+                                               _mockEmployeeValidationService.Object);
 
-    public override int GetHashCode()
-    {
-        return base.GetHashCode();
-    }
+            _request = new CreateBsdRequest
+            {
+                BsdNumber = 1234,
+                EmployeeRegistrations = new List<int> { 1234, 5678 },
+                DateService = "01/01/2024",
+                Digit = 8
+            };
+        }
 
-    [Fact]
-    public async void Post_ReturnsCreatedAtActionResult_WhenBSDIsSuccessfullyInserted()
-    {
-        // Arrange
-        var employee = new Employee { Registration = 1234 };
-        var createdBsd = new CreateBsdRequest();
+        [Fact]
+        public async Task Post_ReturnsCreatedAtActionResult_WhenBSDIsSuccessfullyInserted()
+        {
+            // Arrange
+            SetupEmployeeValidationService(true);
+            SetupBsdApplicationService(_request, new CreateBsdRequest());
 
-        _mockEmployeeRepository
-            .Setup(repo => repo.GetEmployeeByRegistrationAsync(_request.EmployeeRegistration))
-            .ReturnsAsync(employee);
+            // Act
+            var result = await _bsdController.Post(_request);
 
-        _mockBsdApplication
-        .Setup(app => app.CreateBsdAsync(_request))
-        .ReturnsAsync(createdBsd);
+            // Assert
+            var createdAtActionResult = Assert.IsType<CreatedAtActionResult>(result);
+            Assert.Equal(StatusCodes.Status201Created, createdAtActionResult.StatusCode);
+        }
 
-        // Act
-        var result = await _bsdController.Post(_request);
+        [Fact]
+        public async Task Post_ReturnsBadRequestObjectResult_WhenEmployeeNotFound()
+        {
+            // Arrange
+            SetupEmployeeValidationService(false);
 
-        // Assert
-        var createdAtActionResult = Assert.IsType<CreatedAtActionResult>(result);
-        Assert.Equal(201, createdAtActionResult.StatusCode);
-    }
+            // Act
+            var result = await _bsdController.Post(_request);
 
-    [Fact]
-    public async void Post_ReturnsBadRequestObjectResult_WhenEmployeeNotFound()
-    {
-        // Arrange & Act
-        var result = await _bsdController.Post(_request);
+            // Assert
+            var expectedErrorMessage = "Funcionários com as matrículas 1234, 5678 não encontrados.";
+            var badRequestResult = Assert.IsType<BadRequestObjectResult>(result);
+            Assert.Equal(StatusCodes.Status400BadRequest, badRequestResult.StatusCode);
+            Assert.Equal(expectedErrorMessage, badRequestResult.Value);
+        }
 
-        // Assert
-        var badRequestResult = Assert.IsAssignableFrom<BadRequestObjectResult>(result);
-        Assert.Equal(400, badRequestResult.StatusCode);
-        Assert.Equal("Funcionário com a matrícula 1235 não encontrado.", badRequestResult.Value);
-    }
+        [Fact]
+        public async Task Post_ReturnsInternalServerError_WhenExceptionIsThrown()
+        {
+            // Arrange
+            SetupEmployeeValidationService(true);
+            SetupBsdApplicationService(_request, exception: new ApplicationException("Ocorreu um erro interno. Por favor, tente novamente."));
 
-    [Fact]
-    public async Task Post_ReturnsInternalServerError_WhenExceptionIsThrown()
-    {
-        // Arrange & Act
-        var employee = new Employee { Registration = 1234 };
+            // Act
+            var result = await _bsdController.Post(_request);
 
-        _mockEmployeeRepository
-            .Setup(repo => repo.GetEmployeeByRegistrationAsync(_request.EmployeeRegistration))
-            .ReturnsAsync(employee);
-        _mockBsdApplication.Setup(app => app.CreateBsdAsync(_request))
-                            .ThrowsAsync(new ApplicationException(
-                                "Ocorreu um erro interno. Por favor, tente novamente."));
+            // Assert
+            var statusCodeResult = Assert.IsType<ObjectResult>(result);
+            Assert.Equal(StatusCodes.Status500InternalServerError, statusCodeResult.StatusCode);
+            Assert.Equal("Ocorreu um erro interno. Por favor, tente novamente.", statusCodeResult.Value);
+        }
 
-        var result = await _bsdController.Post(_request);
+        private void SetupEmployeeValidationService(bool isValid)
+        {
+            _mockEmployeeValidationService
+                .Setup(service => service.ValidateEmployeeRegistrationsAsync(It.IsAny<List<int>>()))
+                .ReturnsAsync(isValid);
+        }
 
-
-        // Assert
-        var statusCodeResult = Assert.IsType<ObjectResult>(result);
-        Assert.Equal(StatusCodes.Status500InternalServerError, statusCodeResult.StatusCode);
-        Assert.Equal("Ocorreu um erro interno. Por favor, tente novamente.", statusCodeResult.Value);
+        private void SetupBsdApplicationService(CreateBsdRequest request, CreateBsdRequest? response = null, Exception? exception = null)
+        {
+            if (exception != null)
+            {
+                _mockBsdApplication.Setup(app => app.CreateBsdAsync(request)).ThrowsAsync(exception);
+            }
+            else
+            {
+                _mockBsdApplication.Setup(app => app.CreateBsdAsync(request)).ReturnsAsync(response!);
+            }
+        }
     }
 }
