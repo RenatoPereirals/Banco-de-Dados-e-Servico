d5fd9ef435258504fab7258cdf7fed4a8dbecd02
renatoPreirals <renatopreirals@gmail.com>
2024-06-11 13:45:42 -0300

    feat: update IRubricService interface to include FilterRubricsByServiceTypeAndDayAsync method
    - Modified IRubricService interface
      - Added FilterRubricsByServiceTypeAndDayAsync method to retrieve rubrics based on service type and day type

--- src/Bsd.Domain/Services/Interfaces/IRubricService.cs
+++ src/Bsd.Domain/Services/Interfaces/IRubricService.cs
@@ -5,6 +5,6 @@ namespace Bsd.Domain.Services.Interfaces
 {
     public interface IRubricService
     {
-        Task<List<EmployeeRubricHours>> GetEmployeeRubricHoursAsync(DateTime startDate, DateTime endDate);
+        Task<IEnumerable<Rubric>> FilterRubricsByServiceTypeAndDayAsync(ServiceType serviceType, DayType dayType);
     }
 }
