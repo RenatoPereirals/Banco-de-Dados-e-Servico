0d9812695afda47589d823680ab1d28f8eebd90d
renatoPreirals <renatopreirals@gmail.com>
2024-06-11 13:43:05 -0300

    feat: implement AssociateRubricsToEmployeesAsync method in BsdService
    - Modified BsdService class
      - Added AssociateRubricsToEmployeesAsync method to associate rubrics with employees within a BSD entity

--- src/Bsd.Domain/Services/BsdService.cs
+++ src/Bsd.Domain/Services/BsdService.cs
@@ -1,3 +1,5 @@
+using Bsd.Domain.Entities;
+using Bsd.Domain.Enums;
 using Bsd.Domain.Repository.Interfaces;
 using Bsd.Domain.Services.Interfaces;
 
@@ -5,5 +7,45 @@ namespace Bsd.Domain.Services
 {
     public class BsdService : IBsdService
     {
+        private readonly IEmployeeRepository _employeeRepository;
+        private readonly IRubricRepository _rubricRepository;
+
+        public BsdService(IEmployeeRepository employeeRepository,
+                          IRubricRepository rubricRepository)
+        {
+            _rubricRepository = rubricRepository;
+            _employeeRepository = employeeRepository;
+        }
+        public async Task<ICollection<EmployeeRubric>> AssociateRubricsToEmployeesAsync(BsdEntity bsd, DayType day)
+        {
+            var employeeRubricsList = new List<EmployeeRubric>();
+
+            foreach (var employee in bsd.Employees)
+            {
+                if (employee != null)
+                {
+                    var employeeFromRepo = await _employeeRepository.GetEmployeeByRegistrationAsync(employee.EmployeeId);
+
+                    if (employeeFromRepo != null)
+                    {
+                        var filteredRubrics = await _rubricRepository.GetRubricsByServiceTypeAndDayTypeAsync(employeeFromRepo.ServiceType, day);
+
+                        foreach (var rubric in filteredRubrics)
+                        {
+                            employeeRubricsList.Add(new EmployeeRubric
+                            {
+                                BsdEntityId = bsd.BsdId,
+                                BsdEntity = bsd,
+                                EmployeeId = employee.EmployeeId,
+                                Employee = employeeFromRepo,
+                                RubricId = rubric.RubricId,
+                                Rubric = rubric
+                            });
+                        }
+                    }
+                }
+            }
+            return employeeRubricsList;
+        }
     }
 }
