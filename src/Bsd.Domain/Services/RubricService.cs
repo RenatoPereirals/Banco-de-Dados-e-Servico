9a233226f67d61c3bcfac0a4f6e2b79c0225daa8
renatoPreirals <renatopreirals@gmail.com>
2024-06-11 13:44:18 -0300

    feat: update RubricService class to remove unnecessary methods and dependencies
    - Modified RubricService class
      - Removed unnecessary methods and dependencies not being used in the current implementation

--- src/Bsd.Domain/Services/RubricService.cs
+++ src/Bsd.Domain/Services/RubricService.cs
@@ -8,58 +8,16 @@ namespace Bsd.Domain.Services
     public class RubricService : IRubricService
     {
         private readonly IRubricRepository _rubricRepository;
-        private readonly IBsdRepository _bsdRepository;
-        private readonly IEmployeeService _employeeService;
 
-        public RubricService(IRubricRepository rubricRepository,
-                             IBsdRepository bsdRepository,
-                             IEmployeeService employeeService)
+        public RubricService(IRubricRepository rubricRepository)
         {
             _rubricRepository = rubricRepository;
-            _bsdRepository = bsdRepository;
-            _employeeService = employeeService;
         }
 
-        public async Task<List<EmployeeRubricHours>> GetEmployeeRubricHoursAsync(DateTime startDate, DateTime endDate)
-        {
-            var bsdEntities = await _bsdRepository.GetBsdEntitiesByDateRangeAsync(startDate, endDate)
-                ?? throw new Exception($"Não foi possível encontrar nenhum BSD entre as datas {startDate} e {endDate}");
-
-            var result = new List<EmployeeRubricHours>();
-
-            var employeeTasks = bsdEntities.SelectMany(bsdEntity => bsdEntity.Employees.Select(async employee =>
-            {
-                var totalDays = await _employeeService.CalculateEmployeeWorkedDays(employee.Registration, startDate, endDate);
-                var rubrics = await FilterRubricsByServiceTypeAndDayAsync(employee.ServiceType, bsdEntity.DayType);
-
-                return rubrics.Select(rubric => new EmployeeRubricHours
-                {
-                    EmployeeRegistration = employee.Registration,
-                    RubricCode = rubric.Code,
-                    TotalHours = GetRubricHour(rubric) * totalDays
-                });
-            }));
-
-            foreach (var employeeRubricHoursTask in employeeTasks)
-            {
-                var employeeRubricHours = await employeeRubricHoursTask;
-                result.AddRange(employeeRubricHours);
-            }
-
-            return result;
-        }
-
-
-        private async Task<IEnumerable<Rubric>> FilterRubricsByServiceTypeAndDayAsync(ServiceType serviceType, DayType dayType)
+        public async Task<IEnumerable<Rubric>> FilterRubricsByServiceTypeAndDayAsync(ServiceType serviceType, DayType dayType)
         {
             var allRubrics = await _rubricRepository.GetAllRubricsAsync();
             return allRubrics.Where(r => r.ServiceType == serviceType && r.DayType == dayType).ToList();
         }
-
-        private static decimal GetRubricHour(Rubric rubric)
-        {
-            return rubric.HoursPerDay;
-        }
-
     }
 }
