7a47e96285dd9959232f9d86d830dd62a968cdec
renatoPreirals <renatopreirals@gmail.com>
2024-06-11 13:41:07 -0300

    refactor: update Rubric class to use RubricId as primary key
    - Modified Rubric class
      - Added RubricId as the primary key with [Key] attribute
      - Removed Code property

--- src/Bsd.Domain/Entities/Rubric.cs
+++ src/Bsd.Domain/Entities/Rubric.cs
@@ -1,10 +1,12 @@
+using System.ComponentModel.DataAnnotations;
 using Bsd.Domain.Enums;
 
 namespace Bsd.Domain.Entities
 {
     public class Rubric
     {
-        public string Code { get; set; } = string.Empty;
+        [Key]
+        public int RubricId { get; set; }
         public string Description { get; set; } =string.Empty;
         public decimal HoursPerDay { get; set; }
         public DayType DayType { get; set; }
