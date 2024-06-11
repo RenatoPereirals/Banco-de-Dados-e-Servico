af019dce3066fd39e49240a04835d15bacc1dc35
renatoPreirals <renatopreirals@gmail.com>
2024-06-11 13:41:41 -0300

    feat: update IBsdRepository interface to include AddEmployeeToBsdAsync method
    - Modified IBsdRepository interface
      - Added AddEmployeeToBsdAsync method to associate employees with BSD entities

--- src/Bsd.Domain/Repository/Interfaces/IBsdRepository.cs
+++ src/Bsd.Domain/Repository/Interfaces/IBsdRepository.cs
@@ -7,6 +7,7 @@ namespace Bsd.Domain.Repository.Interfaces
         Task<BsdEntity> GetBsdByIdAsync(int bsdId);
         Task<IEnumerable<BsdEntity>> GetAllBsdAsync();
         Task<IEnumerable<BsdEntity>> GetBsdEntitiesByDateRangeAsync(DateTime startDate, DateTime endDate);
+        Task<ICollection<Employee>> AddEmployeeToBsdAsync(BsdEntity bsd);
         Task<bool> CreateBsdAsync(BsdEntity bsd);
     }
 }
