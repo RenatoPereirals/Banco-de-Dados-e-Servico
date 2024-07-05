6292202aeadd47a5d1675c74010cc276838c08c9
renatoPreirals <renatopreirals@gmail.com>
2024-07-05 15:25:34 -0300

    test: refactor VariableDateHolidayAdjusterTest with comprehensive holiday checks
    - Refactored VariableDateHolidayAdjusterTest to comprehensively test holiday calculation methods including Portuary Day, Easter, Good Friday, and Corpus Christi.
    - Updated tests to verify correct behavior for both holiday and non-holiday dates using VariableDateHolidayAdjuster.

--- test/UnitTest/Domain/Services/VariableDateHolidayAdjusterTest.cs
+++ test/UnitTest/Domain/Services/VariableDateHolidayAdjusterTest.cs
@@ -11,11 +11,13 @@ namespace test.Domain.Services
             _variableDateHolidayAdjuster = new VariableDateHolidayAdjuster();
         }
 
+        #region IsPortuaryDay Tests
+
         [Theory]
         [InlineData("28/1/2025")]
         [InlineData("29/1/2024")]
         [InlineData("30/1/2034")]
-        public void Should_Return_True_For_Variable_Date_Holiday(string date)
+        public void IsVariableHoliday_ReturnsTrue_ForPortuaryDay(string date)
         {
             // Arrange
             var portuaryDay = DateTime.Parse(date);
@@ -24,20 +26,133 @@ namespace test.Domain.Services
             var result = _variableDateHolidayAdjuster.IsVariableHoliday(portuaryDay);
 
             // Assert
-            Assert.True(result);
+            Assert.True(result, $"{date} should be a Portuary Day.");
         }
 
-        [Fact]
-        public void Should_Return_False_For_Not_Variable_Date_Holiday()
+        [Theory]
+        [InlineData("28/1/2024")]
+        [InlineData("15/8/2024")]
+        [InlineData("10/3/2034")]
+        public void IsVariableHoliday_ReturnsFalse_ForNonPortuaryDay(string date)
         {
             // Arrange
-            var portuaryDay = DateTime.Parse("28/1/2024");
+            var nonPortuaryDay = DateTime.Parse(date);
 
             // Act
-            var result = _variableDateHolidayAdjuster.IsVariableHoliday(portuaryDay);
+            var result = _variableDateHolidayAdjuster.IsVariableHoliday(nonPortuaryDay);
 
             // Assert
-            Assert.False(result);
+            Assert.False(result, $"{date} should not be a Portuary Day.");
         }
+
+        #endregion
+
+        #region IsEaster Tests
+
+        [Theory]
+        [InlineData("4/4/2021")]
+        [InlineData("17/4/2022")]
+        [InlineData("9/4/2023")]
+        public void IsVariableHoliday_ReturnsTrue_ForEaster(string date)
+        {
+            // Arrange
+            var easterDate = DateTime.Parse(date);
+
+            // Act
+            var result = _variableDateHolidayAdjuster.IsVariableHoliday(easterDate);
+
+            // Assert
+            Assert.True(result, $"{date} should be Easter.");
+        }
+
+        [Theory]
+        [InlineData("30/3/2025")]
+        [InlineData("1/1/2026")]
+        [InlineData("20/5/2027")]
+        public void IsVariableHoliday_ReturnsFalse_ForNonEaster(string date)
+        {
+            // Arrange
+            var nonEasterDate = DateTime.Parse(date);
+
+            // Act
+            var result = _variableDateHolidayAdjuster.IsVariableHoliday(nonEasterDate);
+
+            // Assert
+            Assert.False(result, $"{date} should not be Easter.");
+        }
+
+        #endregion
+
+        #region IsGoodFriday Tests
+
+        [Theory]
+        [InlineData("2/4/2021")]
+        [InlineData("15/4/2022")]
+        [InlineData("7/4/2023")]
+        public void IsVariableHoliday_ReturnsTrue_ForGoodFriday(string date)
+        {
+            // Arrange
+            var goodFridayDate = DateTime.Parse(date);
+
+            // Act
+            var result = _variableDateHolidayAdjuster.IsVariableHoliday(goodFridayDate);
+
+            // Assert
+            Assert.True(result, $"{date} should be Good Friday.");
+        }
+
+        [Theory]
+        [InlineData("30/4/2025")]
+        [InlineData("1/1/2026")]
+        [InlineData("20/5/2027")]
+        public void IsVariableHoliday_ReturnsFalse_ForNonGoodFriday(string date)
+        {
+            // Arrange
+            var nonGoodFridayDate = DateTime.Parse(date);
+
+            // Act
+            var result = _variableDateHolidayAdjuster.IsVariableHoliday(nonGoodFridayDate);
+
+            // Assert
+            Assert.False(result, $"{date} should not be Good Friday.");
+        }
+
+        #endregion
+
+        #region IsCorpusChristi Tests
+
+        [Theory]
+        [InlineData("3/6/2021")]
+        [InlineData("16/6/2022")]
+        [InlineData("8/6/2023")]
+        public void IsVariableHoliday_ReturnsTrue_ForCorpusChristi(string date)
+        {
+            // Arrange
+            var corpusChristiDate = DateTime.Parse(date);
+
+            // Act
+            var result = _variableDateHolidayAdjuster.IsVariableHoliday(corpusChristiDate);
+
+            // Assert
+            Assert.True(result, $"{date} should be Corpus Christi.");
+        }
+
+        [Theory]
+        [InlineData("30/4/2025")]
+        [InlineData("1/1/2026")]
+        [InlineData("20/5/2027")]
+        public void IsVariableHoliday_ReturnsFalse_ForNonCorpusChristi(string date)
+        {
+            // Arrange
+            var nonCorpusChristiDate = DateTime.Parse(date);
+
+            // Act
+            var result = _variableDateHolidayAdjuster.IsVariableHoliday(nonCorpusChristiDate);
+
+            // Assert
+            Assert.False(result, $"{date} should not be Corpus Christi.");
+        }
+
+        #endregion
     }
 }
