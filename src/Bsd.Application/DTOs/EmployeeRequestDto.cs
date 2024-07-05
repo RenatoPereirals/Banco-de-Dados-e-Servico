14509ba627fb1844187e44c628b60e166de520e0
renatoPreirals <renatopreirals@gmail.com>
2024-07-05 15:02:00 -0300

    feat: create EmployeeRequestDto for employee data transport
    - Added EmployeeRequestDto class to facilitate data transport in controllers.
    - Includes properties for EmployeeId and ServiceType with appropriate data types.

---
+++ src/Bsd.Application/DTOs/EmployeeRequestDto.cs
@@ -0,0 +1,8 @@
+namespace Bsd.Application.DTOs
+{
+    public class EmployeeRequestDto
+    {
+        public int EmployeeId { get; set; }
+        public string ServiceType { get; set; } = string.Empty;
+    }
+}
