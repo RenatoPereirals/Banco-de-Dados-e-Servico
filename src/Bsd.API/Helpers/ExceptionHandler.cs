aa9259daf7b5fc80c589b91b22324a2b8e4d2b5d
renatoPreirals <renatopreirals@gmail.com>
2024-07-05 15:00:42 -0300

    feat: enhance exception handling in ExceptionHandler
    - Introduced custom exceptions (NotFoundException, UnauthorizedAccessException).
    - Updated HandleExceptionAsync method to handle specific exception types with appropriate HTTP status codes and error messages.
    - Refactored error response generation for clarity and consistency.
    - Improved error handling strategy based on environment (Development or Qa vs. other environments).
    - Implemented JSON serialization for error responses.
    - Changes are temporary and require further testing for validation.

--- src/Bsd.API/Helpers/ExceptionHandler.cs
+++ src/Bsd.API/Helpers/ExceptionHandler.cs
@@ -9,24 +9,44 @@ namespace Bsd.API.Helpers
         public static Task HandleExceptionAsync(HttpContext context, Exception ex)
         {
             ErrorResponseVm errorResponseVm;
+            HttpStatusCode statusCode;
 
-            if (Environment.GetEnvironmentVariable("ASPNETCORE_ENVIRONMENT") == "Development" ||
-                Environment.GetEnvironmentVariable("ASPNETCORE_ENVIRONMENT") == "Qa")
+            switch (ex)
             {
-                errorResponseVm = new ErrorResponseVm(HttpStatusCode.InternalServerError.ToString(),
-                                                      $"{ex.Message} {ex?.InnerException?.Message}");
-            }
-            else
-            {
-                errorResponseVm = new ErrorResponseVm(HttpStatusCode.InternalServerError.ToString(),
-                                                      "An internal server error has occurred.");
+                case NotFoundException notFoundException:
+                    statusCode = HttpStatusCode.NotFound;
+                    errorResponseVm = new ErrorResponseVm(statusCode.ToString(), notFoundException.Message);
+                    break;
+                case UnauthorizedAccessException unauthorizedAccessException:
+                    statusCode = HttpStatusCode.Unauthorized;
+                    errorResponseVm = new ErrorResponseVm(statusCode.ToString(), unauthorizedAccessException.Message);
+                    break;
+                default:
+                    statusCode = HttpStatusCode.InternalServerError;
+                    if (Environment.GetEnvironmentVariable("ASPNETCORE_ENVIRONMENT") == "Development" ||
+                        Environment.GetEnvironmentVariable("ASPNETCORE_ENVIRONMENT") == "Qa")
+                    {
+                        errorResponseVm = new ErrorResponseVm(statusCode.ToString(),
+                                                              $"{ex.Message} {ex?.InnerException?.Message}");
+                    }
+                    else
+                    {
+                        errorResponseVm = new ErrorResponseVm(statusCode.ToString(),
+                                                              "An internal server error has occurred.");
+                    }
+                    break;
             }
 
-            context.Response.StatusCode = (int)HttpStatusCode.InternalServerError;
+            context.Response.StatusCode = (int)statusCode;
 
             var result = JsonConvert.SerializeObject(errorResponseVm);
             context.Response.ContentType = "application/json";
             return context.Response.WriteAsync(result);
         }
+    }
+
+    public class NotFoundException : Exception
+    {
+        public NotFoundException(string message) : base(message) { }
     }
 }
