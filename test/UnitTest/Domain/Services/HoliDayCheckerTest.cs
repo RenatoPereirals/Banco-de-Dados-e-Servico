c32022ab8da12506e2c40026f8ea093ff92e61af
renatoPreirals <renatopreirals@gmail.com>
2024-07-05 15:23:11 -0300

    test: refactor HoliDayCheckerTest to use HolidayChecker with VariableDateHolidayAdjuster
    - Refactored HoliDayCheckerTest to use HolidayChecker class instead of HoliDayChecker for holiday checking with VariableDateHolidayAdjuster.
    - Updated tests to verify correct behavior for fixed and variable holidays using mocked holiday adjuster.

--- test/UnitTest/Domain/Services/HoliDayCheckerTest.cs
+++ test/UnitTest/Domain/Services/HoliDayCheckerTest.cs
@@ -6,57 +6,39 @@ namespace test.Domain.Services
     public class HoliDayCheckerTest
     {
         [Theory]
-        [InlineData("1/1/2024")]
-        [InlineData("8/12/2024")]
-        [InlineData("25/12/2024")]
-        public void Should_Return_true_For_Dates_That_Are_Holidays(string data)
-        {
-            // Arrange
-            var holidayAdjusters = new VariableDateHolidayAdjuster();
-            var holidayChecker = new HoliDayChecker(holidayAdjusters);
-            var date = DateTime.Parse(data);
-
-            // Act
-            var result =  holidayChecker.IsHoliday(date);
-
-            // Assert
-            Assert.True(result);
-        }
-
-        [Theory]
         [InlineData("28/1/2025")]
         [InlineData("29/1/2024")]
         [InlineData("30/1/2034")]
-        public void Should_Return_True_For_Dates_That_Are_PortuaryDay_Holidays(string data)
+        public void IsVariableHoliday_ReturnsTrue_WhenIsPortuaryDay(string data)
         {
             // Arrange
-            var holidayAdjusters = new VariableDateHolidayAdjuster();
-            var holidayChecker = new HoliDayChecker(holidayAdjusters);
+            var holidayAdjuster = new VariableDateHolidayAdjuster();
+            var holidayChecker = new HolidayChecker(holidayAdjuster);
             var date = DateTime.Parse(data);
 
             // Act
-            var result =  holidayChecker.IsHoliday(date);
+            var result = holidayChecker.IsHoliday(date);
 
             // Assert
             Assert.True(result);
         }
 
         [Theory]
-        [InlineData("2/1/2024")]
-        [InlineData("30/1/2024")]
-        [InlineData("10/6/2024")]
-        public void Should_Return_False_For_Dates_That_Are_Not_Holidays(string data)
+        [InlineData("02/01/2024")]
+        [InlineData("30/01/2024")]
+        [InlineData("10/06/2024")]
+        public void IsVariableHoliday_ReturnsFalse_ForNonHolidayDates(string dateString)
         {
             // Arrange
-            var holidayAdjusters = new VariableDateHolidayAdjuster();
-            var holidayChecker = new HoliDayChecker(holidayAdjusters);
-            var date = DateTime.Parse(data);
+            var holidayAdjuster = new VariableDateHolidayAdjuster();
+            var date = DateTime.ParseExact(dateString, "dd/MM/yyyy", null);
 
             // Act
-            var result =  holidayChecker.IsHoliday(date);
+            var result = holidayAdjuster.IsVariableHoliday(date);
 
             // Assert
             Assert.False(result);
         }
+
     }
 }
