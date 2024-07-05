3d7ecb5455b5e254b8a26a48337c896f21d2be03
renatoPreirals <renatopreirals@gmail.com>
2024-07-05 15:08:35 -0300

    refactor: unify and optimize employee creation logic in EmployeeApplicationService
    - Combined and simplified logic for creating employees with either registration and service type or EmployeeRequestDto.
    - Enhanced error handling and validation for employee creation parameters.
    - Ensured consistent error messaging and exception handling throughout the service.

--- src/Bsd.Application/Services/EmployeeApplicationService.cs
+++ src/Bsd.Application/Services/EmployeeApplicationService.cs
@@ -1,47 +1,79 @@
 
+using AutoMapper;
+
 using Bsd.Application.Interfaces;
+using Bsd.Application.DTOs;
+
+using Bsd.Domain.Services.Interfaces;
+using Bsd.Domain.Repository.Interfaces;
 using Bsd.Domain.Entities;
 using Bsd.Domain.Enums;
-using Bsd.Domain.Repository.Interfaces;
-using Bsd.Domain.Services.Interfaces;
 
 namespace Bsd.Application.Services
 {
     public class EmployeeApplicationService : IEmployeeApplicationService
     {
         private readonly IEmployeeService _employeeService;
-        private readonly IGeralRepository _geralRepository;
+        private readonly IEmployeeRepository _employeeRepository;
+        private readonly IMapper _mapper;
 
-        public EmployeeApplicationService(IGeralRepository geralRepository,
-                                          IEmployeeService employeeService)
+        public EmployeeApplicationService(IEmployeeService employeeService,
+                                          IEmployeeRepository employeeRepository,
+                                          IMapper mapper)
         {
             _employeeService = employeeService;
-            _geralRepository = geralRepository;
+            _employeeRepository = employeeRepository;
+            _mapper = mapper;
         }
 
-        public async Task CreateEmployeeAsync(int registration, string serviceType)
+        public async Task<EmployeeRequestDto> CreateEmployeeAsync(EmployeeRequestDto request)
         {
-            try
+            RequestValidation(request);
+
+            var employee = _mapper.Map<Employee>(request);
+
+            var serviceType = request.ServiceType.Replace(" ", "");
+
+            if (!Enum.TryParse(serviceType, out ServiceType service))
             {
-                serviceType = serviceType.Replace(" ", "");
-
-                if (!Enum.TryParse(serviceType, out ServiceType service))
-                {
-                    throw new ArgumentException($"O tipo de serviço {serviceType} não existe.", nameof(serviceType));
-                }
-
-                var digit = _employeeService.CalculateModulo11CheckDigit(registration);
-
-                var employee = new Employee();
-                _geralRepository.Create(employee);
-
-                await _geralRepository.SaveChangesAsync();
-
+                throw new ArgumentException($"O tipo de serviço {serviceType} não existe.", nameof(serviceType));
             }
-            catch (ArgumentException ex)
-            {
 
-                throw new Exception($"Erro ao tentar cria funcionário. Error: {ex.Message}");
+            var digit = _employeeService.CalculateModulo11CheckDigit(request.EmployeeId);
+
+            employee.Digit = digit;
+
+            if (await _employeeRepository.CreateEmployeeAsync(employee))
+            {
+                var employeeReturn = await _employeeRepository.GetEmployeeByRegistrationAsync(employee.EmployeeId);
+                return _mapper.Map<EmployeeRequestDto>(employeeReturn);
+            }
+
+            throw new ApplicationException("Falha ao criar funcionário no banco de dados.");
+        }
+
+        private static void RequestValidation(EmployeeRequestDto request)
+        {
+            if (request == null)
+            {
+                throw new ArgumentNullException(nameof(request), "Você precisa preencher todos os dados.");
+            }
+
+            var bsdNumer = request.EmployeeId.ToString();
+
+            if (bsdNumer.Length != 4)
+            {
+                throw new ArgumentOutOfRangeException("O número da matrícula deve conter 4 digitos.", nameof(request));
+            }
+
+            if (request.EmployeeId <= 0)
+            {
+                throw new ArgumentOutOfRangeException("O número da matrícula não pode ser negativo.", nameof(request));
+            }
+
+            if (string.IsNullOrWhiteSpace(request.ServiceType))
+            {
+                throw new ArgumentException("O serviço não pode ser vazio ou nulo.", nameof(request));
             }
         }
     }
