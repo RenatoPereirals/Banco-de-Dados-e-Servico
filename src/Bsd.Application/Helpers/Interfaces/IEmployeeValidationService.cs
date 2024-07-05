b094ab726aef4eb37d28956df14ea7c404cd44ff
renatoPreirals <renatopreirals@gmail.com>
2024-07-05 15:03:37 -0300

    feat: expand IEmployeeValidationService interface for employee registration validation
    - Added ValidateEmployeeRegistrationAsync method to IEmployeeValidationService interface.
    - Updated interface to include methods for validating both lists and single employee registrations.

--- src/Bsd.Application/Helpers/Interfaces/IEmployeeValidationService.cs
+++ src/Bsd.Application/Helpers/Interfaces/IEmployeeValidationService.cs
@@ -3,5 +3,6 @@ namespace Bsd.Application.Helpers.Interfaces
     public interface IEmployeeValidationService
     {
         Task<bool> ValidateEmployeeRegistrationsAsync(List<int> employeeRegistrations);
+        Task<bool> ValidateEmployeeRegistrationAsync(int employeeRegistrations);
     }
 }
