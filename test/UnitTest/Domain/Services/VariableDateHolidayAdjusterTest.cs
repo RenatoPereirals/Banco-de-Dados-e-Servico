96a6487ae0e5e68445cb61c1dce3c1c8fe62c02e
RenatoPereirals <renatopreirals@gmail.com>
2024-07-06 17:12:08 -0300

    test: refactor VariableDateHolidayAdjusterTest for improved date parsing and clarity
    - Introduced `DateHelper` for consistent date parsing across test cases.
    - Updated test cases to use `DateHelper.ParseDate` for date conversion.
    - Renamed `date` parameter to `dateString` for clarity and consistency.
    - Organized using directives and removed unnecessary ones.

--- test/UnitTest/Domain/Services/VariableDateHolidayAdjusterTest.cs
+++ test/UnitTest/Domain/Services/VariableDateHolidayAdjusterTest.cs
@@ -1,3 +1,5 @@
+using Bsd.Application.Helpers;
+
 using Bsd.Domain.Service;
 
 namespace test.Domain.Services
@@ -5,44 +7,46 @@ namespace test.Domain.Services
     public class VariableDateHolidayAdjusterTest
     {
         private readonly VariableDateHolidayAdjuster _variableDateHolidayAdjuster;
+        private readonly DateHelper _dateHelper;
 
         public VariableDateHolidayAdjusterTest()
         {
             _variableDateHolidayAdjuster = new VariableDateHolidayAdjuster();
+            _dateHelper = new DateHelper();
         }
 
         #region IsPortuaryDay Tests
 
         [Theory]
-        [InlineData("28/1/2025")]
-        [InlineData("29/1/2024")]
-        [InlineData("30/1/2034")]
-        public void IsVariableHoliday_ReturnsTrue_ForPortuaryDay(string date)
+        [InlineData("28/01/2025")]
+        [InlineData("29/01/2024")]
+        [InlineData("30/01/2034")]
+        public void IsVariableHoliday_ReturnsTrue_ForPortuaryDay(string dateString)
         {
             // Arrange
-            var portuaryDay = DateTime.Parse(date);
+            var portuaryDay = _dateHelper.ParseDate(dateString);
 
             // Act
             var result = _variableDateHolidayAdjuster.IsVariableHoliday(portuaryDay);
 
             // Assert
-            Assert.True(result, $"{date} should be a Portuary Day.");
+            Assert.True(result, $"{dateString} should be a Portuary Day.");
         }
 
         [Theory]
-        [InlineData("28/1/2024")]
-        [InlineData("15/8/2024")]
-        [InlineData("10/3/2034")]
-        public void IsVariableHoliday_ReturnsFalse_ForNonPortuaryDay(string date)
+        [InlineData("28/01/2024")]
+        [InlineData("15/08/2024")]
+        [InlineData("10/03/2034")]
+        public void IsVariableHoliday_ReturnsFalse_ForNonPortuaryDay(string dateString)
         {
             // Arrange
-            var nonPortuaryDay = DateTime.Parse(date);
+            var nonPortuaryDay = _dateHelper.ParseDate(dateString);
 
             // Act
             var result = _variableDateHolidayAdjuster.IsVariableHoliday(nonPortuaryDay);
 
             // Assert
-            Assert.False(result, $"{date} should not be a Portuary Day.");
+            Assert.False(result, $"{dateString} should not be a Portuary Day.");
         }
 
         #endregion
@@ -53,32 +57,32 @@ namespace test.Domain.Services
         [InlineData("4/4/2021")]
         [InlineData("17/4/2022")]
         [InlineData("9/4/2023")]
-        public void IsVariableHoliday_ReturnsTrue_ForEaster(string date)
+        public void IsVariableHoliday_ReturnsTrue_ForEaster(string dateString)
         {
             // Arrange
-            var easterDate = DateTime.Parse(date);
+            var easterDate = _dateHelper.ParseDate(dateString);
 
             // Act
             var result = _variableDateHolidayAdjuster.IsVariableHoliday(easterDate);
 
             // Assert
-            Assert.True(result, $"{date} should be Easter.");
+            Assert.True(result, $"{dateString} should be Easter.");
         }
 
         [Theory]
-        [InlineData("30/3/2025")]
-        [InlineData("1/1/2026")]
-        [InlineData("20/5/2027")]
-        public void IsVariableHoliday_ReturnsFalse_ForNonEaster(string date)
+        [InlineData("30/03/2025")]
+        [InlineData("01/01/2026")]
+        [InlineData("20/05/2027")]
+        public void IsVariableHoliday_ReturnsFalse_ForNonEaster(string dateString)
         {
             // Arrange
-            var nonEasterDate = DateTime.Parse(date);
+            var nonEasterDate = _dateHelper.ParseDate(dateString);
 
             // Act
             var result = _variableDateHolidayAdjuster.IsVariableHoliday(nonEasterDate);
 
             // Assert
-            Assert.False(result, $"{date} should not be Easter.");
+            Assert.False(result, $"{dateString} should not be Easter.");
         }
 
         #endregion
@@ -86,35 +90,35 @@ namespace test.Domain.Services
         #region IsGoodFriday Tests
 
         [Theory]
-        [InlineData("2/4/2021")]
-        [InlineData("15/4/2022")]
-        [InlineData("7/4/2023")]
-        public void IsVariableHoliday_ReturnsTrue_ForGoodFriday(string date)
+        [InlineData("02/04/2021")]
+        [InlineData("15/04/2022")]
+        [InlineData("07/04/2023")]
+        public void IsVariableHoliday_ReturnsTrue_ForGoodFriday(string dateString)
         {
             // Arrange
-            var goodFridayDate = DateTime.Parse(date);
+            var goodFridayDate = _dateHelper.ParseDate(dateString);
 
             // Act
             var result = _variableDateHolidayAdjuster.IsVariableHoliday(goodFridayDate);
 
             // Assert
-            Assert.True(result, $"{date} should be Good Friday.");
+            Assert.True(result, $"{dateString} should be Good Friday.");
         }
 
         [Theory]
-        [InlineData("30/4/2025")]
-        [InlineData("1/1/2026")]
-        [InlineData("20/5/2027")]
-        public void IsVariableHoliday_ReturnsFalse_ForNonGoodFriday(string date)
+        [InlineData("30/04/2025")]
+        [InlineData("01/01/2026")]
+        [InlineData("20/05/2027")]
+        public void IsVariableHoliday_ReturnsFalse_ForNonGoodFriday(string dateString)
         {
             // Arrange
-            var nonGoodFridayDate = DateTime.Parse(date);
+            var nonGoodFridayDate = _dateHelper.ParseDate(dateString);
 
             // Act
             var result = _variableDateHolidayAdjuster.IsVariableHoliday(nonGoodFridayDate);
 
             // Assert
-            Assert.False(result, $"{date} should not be Good Friday.");
+            Assert.False(result, $"{dateString} should not be Good Friday.");
         }
 
         #endregion
@@ -122,35 +126,35 @@ namespace test.Domain.Services
         #region IsCorpusChristi Tests
 
         [Theory]
-        [InlineData("3/6/2021")]
-        [InlineData("16/6/2022")]
-        [InlineData("8/6/2023")]
-        public void IsVariableHoliday_ReturnsTrue_ForCorpusChristi(string date)
+        [InlineData("03/06/2021")]
+        [InlineData("16/06/2022")]
+        [InlineData("08/06/2023")]
+        public void IsVariableHoliday_ReturnsTrue_ForCorpusChristi(string dateString)
         {
             // Arrange
-            var corpusChristiDate = DateTime.Parse(date);
+            var corpusChristiDate = _dateHelper.ParseDate(dateString);
 
             // Act
             var result = _variableDateHolidayAdjuster.IsVariableHoliday(corpusChristiDate);
 
             // Assert
-            Assert.True(result, $"{date} should be Corpus Christi.");
+            Assert.True(result, $"{dateString} should be Corpus Christi.");
         }
 
         [Theory]
-        [InlineData("30/4/2025")]
-        [InlineData("1/1/2026")]
-        [InlineData("20/5/2027")]
-        public void IsVariableHoliday_ReturnsFalse_ForNonCorpusChristi(string date)
+        [InlineData("30/04/2025")]
+        [InlineData("01/01/2026")]
+        [InlineData("20/05/2027")]
+        public void IsVariableHoliday_ReturnsFalse_ForNonCorpusChristi(string dateString)
         {
             // Arrange
-            var nonCorpusChristiDate = DateTime.Parse(date);
+            var nonCorpusChristiDate = _dateHelper.ParseDate(dateString);
 
             // Act
             var result = _variableDateHolidayAdjuster.IsVariableHoliday(nonCorpusChristiDate);
 
             // Assert
-            Assert.False(result, $"{date} should not be Corpus Christi.");
+            Assert.False(result, $"{dateString} should not be Corpus Christi.");
         }
 
         #endregion
