8cee9044c5640a2f0ba68b766f8fc9b4d04a644a
RenatoPereirals <renatopreirals@gmail.com>
2024-07-06 18:04:21 -0300

    refactor: refactor holiday checking tests to use DateTime parsing helper and HolidayChecker integration
    - Modified existing tests to use DateHelper for DateTime parsing in test setup.
    - Consolidated holiday checking logic using HolidayChecker class for integrated tests.
    - Added new tests for holiday eve checking functionality.
    - Updated inline data for both holiday and holiday eve checks to improve coverage and clarity.

--- test/UnitTest/Domain/Services/HoliDayCheckerTest.cs
+++ test/UnitTest/Domain/Services/HoliDayCheckerTest.cs
@@ -1,27 +1,85 @@
+using Bsd.Application.Helpers;
 using Bsd.Domain.Entities;
 using Bsd.Domain.Service;
+using Bsd.Domain.Service.Interfaces;
+using Moq;
 
 namespace test.Domain.Services
 {
     public class HoliDayCheckerTest
     {
+        private readonly Mock<IVariableDateHolidayAdjuster> _variableDate;
+        private readonly DateHelper _dateHelper;
+        private readonly HolidayChecker _holidayChecker;
+
+        public HoliDayCheckerTest()
+        {
+            _variableDate = new Mock<IVariableDateHolidayAdjuster>();
+            _dateHelper = new DateHelper();
+            _holidayChecker = new HolidayChecker(_variableDate.Object);
+        }
+
+        #region IsHoliday Test
+
         [Theory]
-        [InlineData("28/01/2025")]
-        [InlineData("29/01/2024")]
-        [InlineData("30/01/2034")]
-        public void IsVariableHoliday_ReturnsTrue_WhenIsPortuaryDay(string data)
+        [InlineData("01/01/2023")]
+        [InlineData("06/03/2023")]
+        [InlineData("07/04/2023")]
+        [InlineData("21/04/2023")]
+        [InlineData("01/05/2023")]
+        [InlineData("24/06/2023")]
+        [InlineData("16/07/2023")]
+        [InlineData("07/09/2023")]
+        [InlineData("12/10/2023")]
+        [InlineData("02/11/2023")]
+        [InlineData("15/11/2023")]
+        [InlineData("08/12/2023")]
+        [InlineData("25/12/2023")]
+        public void IsVariableHoliday_ReturnsTrue_WhenIsHoliday(string dateString)
         {
             // Arrange
-            var holidayAdjuster = new VariableDateHolidayAdjuster();
-            var holidayChecker = new HolidayChecker(holidayAdjuster);
-            var date = DateTime.Parse(data);
+            var date = _dateHelper.ParseDate(dateString);
 
             // Act
-            var result = holidayChecker.IsHoliday(date);
+            var result = _holidayChecker.IsHoliday(date);
 
             // Assert
             Assert.True(result);
         }
+
+        #endregion
+
+        #region IsHolidayEve Test
+
+        [Theory]
+        [InlineData("31/12/2023")]
+        [InlineData("05/03/2023")]
+        [InlineData("06/04/2023")]
+        [InlineData("20/04/2023")]
+        [InlineData("30/04/2023")]
+        [InlineData("23/06/2023")]
+        [InlineData("15/07/2023")]
+        [InlineData("06/09/2023")]
+        [InlineData("11/10/2023")]
+        [InlineData("01/11/2023")]
+        [InlineData("14/11/2023")]
+        [InlineData("07/12/2023")]
+        [InlineData("24/12/2023")]
+        public void IsVariableHoliday_ReturnsTrue_WhenIsHolidayEve(string dateString)
+        {
+            // Arrange
+            var date = _dateHelper.ParseDate(dateString);
+
+            // Act
+            var result = _holidayChecker.IsHolidayEve(date);
+
+            // Assert
+            Assert.True(result);
+        }
+
+        #endregion
+
+        #region IsNotHooliday Test
 
         [Theory]
         [InlineData("02/01/2024")]
@@ -30,15 +88,15 @@ namespace test.Domain.Services
         public void IsVariableHoliday_ReturnsFalse_ForNonHolidayDates(string dateString)
         {
             // Arrange
-            var holidayAdjuster = new VariableDateHolidayAdjuster();
-            var date = DateTime.ParseExact(dateString, "dd/MM/yyyy", null);
+            var date = _dateHelper.ParseDate(dateString);
 
             // Act
-            var result = holidayAdjuster.IsVariableHoliday(date);
+            var result = _holidayChecker.IsHoliday(date);
 
             // Assert
             Assert.False(result);
         }
 
+        #endregion
     }
 }
