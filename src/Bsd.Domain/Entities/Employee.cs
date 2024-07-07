dad0c06ff88c497f154e4b7d60939730b8998453
RenatoPereirals <renatopreirals@gmail.com>
2024-07-07 16:17:56 -0300

    refactor: Simplify Employee entity by removing unnecessary properties and collections
    - Removed constructor and BsdEntities collection as they are not required.
    - Retained only essential properties: EmployeeId, Digit, and ServiceType.

--- src/Bsd.Domain/Entities/Employee.cs
+++ src/Bsd.Domain/Entities/Employee.cs
@@ -5,16 +5,9 @@ namespace Bsd.Domain.Entities
 {
     public class Employee
     {
-        public Employee()
-        {
-            BsdEntities = new HashSet<BsdEntity>();
-        }
-
         [Key]
         public int EmployeeId { get; set; }
         public int Digit { get; set; }
         public ServiceType ServiceType { get; set; }
-        public DateTime DateService { get; set; }
-        public ICollection<BsdEntity> BsdEntities { get; set; }
     }
 }
