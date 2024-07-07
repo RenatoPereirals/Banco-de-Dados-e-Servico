1627b62ae81afe32bbc7107efdb7382772642fd0
RenatoPereirals <renatopreirals@gmail.com>
2024-07-07 16:16:41 -0300

    refactor: Update validation attributes in CreateBsdRequest DTO
    - Changed error message format for required fields to include the field name.
    - Added RegularExpression attribute for BsdNumber to ensure it contains exactly 6 digits.
    - Added RegularExpression attribute for EmployeeId to ensure it contains exactly 5 digits.
    - Added RegularExpression attribute for Digit to ensure it contains exactly 1 digit.
    - Updated property EmployeeRegistrations to EmployeeId to reflect single employee validation.

--- src/Bsd.Application/DTOs/CreateBsdRequest.cs
+++ src/Bsd.Application/DTOs/CreateBsdRequest.cs
@@ -4,7 +4,8 @@ namespace Bsd.Application.DTOs
 {
     public class CreateBsdRequest
     {
-        [Required(ErrorMessage = "O número do BSD é obrigatório.")]
+        [Required(ErrorMessage = "O campo {0} é obrigatório.")]
+        [RegularExpression(@"^\d{6}$", ErrorMessage = "O campo {0} deve conter 6 dígitos.")]
         public int BsdNumber { get; set; }
 
         [Required(ErrorMessage = "A data do serviço é obrigatória.")]
@@ -13,11 +14,13 @@ namespace Bsd.Application.DTOs
 
         public DateTime DateServiceDate { get; set; }
 
-        [Required(ErrorMessage = "A matrícula do funcionário é obrigatória.")]
-        public List<int> EmployeeRegistrations { get; set; } = new();
+        [Required(ErrorMessage = "O campo {0} é obrigatória.")]
+        [RegularExpression(@"^\d{5}$", ErrorMessage = "O campo {0} deve conter 5 dígitos.")]
+        public int EmployeeId { get; set; }
 
-        [Required(ErrorMessage = "O dígito é obrigatório.")]
-        [Range(0, 9, ErrorMessage = "O dígito deve estar entre 0 e 9.")]
+        [Required(ErrorMessage = "O campo {0} é obrigatório.")]
+        [Range(0, 9, ErrorMessage = "O campo {0} deve estar entre 0 e 9.")]
+        [RegularExpression(@"^\d{1}$", ErrorMessage = "O campo {0} deve conter 1 dígitos.")]
         public int Digit { get; set; }
     }
 }
