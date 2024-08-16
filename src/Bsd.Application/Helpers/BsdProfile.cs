91141c9849c42674743a89a20db3647234547a82
renatoPreirals <renatopreirals@gmail.com>
2024-08-16 16:52:54 -0300

    feat: Update AutoMapper profiles for new mappings
    - Added reverse mapping for `CreateBsdRequest` to `BsdEntity` and vice versa.
    - Added mapping for `MarkResponse` to `BsdEntity` with custom value resolvers and mappings for time properties.
    - Added mapping to convert `BsdEntity` to a list of `ReportResponse` for reporting purposes.

--- src/Bsd.Application/Helpers/BsdProfile.cs
+++ src/Bsd.Application/Helpers/BsdProfile.cs
@@ -1,4 +1,5 @@
 using AutoMapper;
+
 using Bsd.Application.DTOs;
 using Bsd.Domain.Entities;
 
@@ -9,7 +10,28 @@ namespace Bsd.Application.Helpers
         public BsdProfile()
         {
             CreateMap<CreateBsdRequest, BsdEntity>()
-                .ForMember(dest => dest.DateService, opt => opt.MapFrom(src => src.DateServiceDate));
+                .ForMember(dest => dest.DateService, opt => opt.MapFrom(src => src.DateServiceDate))
+                .ReverseMap();
+
+            CreateMap<MarkResponse, BsdEntity>()
+                .ForMember(dest => dest.DateService, opt => opt.MapFrom<DateHelperValueResolver>())
+                .ForMember(dest => dest.StartTime, opt => opt.MapFrom(src => new TimeOnly(src.Hora, src.Minuto)))
+                .ForMember(dest => dest.EndTime, opt => opt.MapFrom(src => new TimeOnly(src.Hora, src.Minuto)))
+                .ForMember(dest => dest.DayType, opt => opt.Ignore())
+                .ForMember(dest => dest.Employees, opt => opt.Ignore())
+                .ReverseMap();
+
+            CreateMap<BsdEntity, List<ReportResponse>>()
+                .ConvertUsing(src => src.Employees.SelectMany(assignment =>
+                    assignment.Rubrics.Select(rubric => new ReportResponse
+                    {
+                        MatriculaPessoa = assignment.EmployeeId,
+                        Rubric = rubric.RubricId,
+                        TotalHours = rubric.TotalWorkedHours
+                    })).ToList());
+
+
+
         }
     }
 }
