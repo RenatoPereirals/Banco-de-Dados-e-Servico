f027b985c81182c4973f681904af8a3c324af83e
renatoPreirals <renatopreirals@gmail.com>
2024-08-16 16:40:27 -0300

    feat: Implement ReportController for report generation
    - Created `ReportController` class in the `Bsd.API.Controllers` namespace.
    - Added `GenerateReport` endpoint for handling report generation requests.
      - HTTP POST method at route `api/report/generate`.
      - Accepts a `ReportRequest` DTO in the request body.
      - Returns:
        - 200 OK if report generation is initiated successfully.
        - 204 No Content if no content is provided.
        - 400 Bad Request if the model state is invalid.
        - 404 Not Found if the requested resource is not found.
        - 500 Internal Server Error if report generation fails.
    - Integrated `IBsdApplicationService` to handle the report generation logic.
    - Included appropriate HTTP status codes and responses to enhance error handling and client feedback.

---
+++ src/Bsd.API/Controllers/ReportController.cs
@@ -0,0 +1,41 @@
+﻿using Bsd.Application.Interfaces;
+using Bsd.Application.DTOs;
+
+using System.Net;
+
+using Microsoft.AspNetCore.Mvc;
+
+namespace Bsd.API.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class ReportController : Controller
+    {
+        private readonly IBsdApplicationService _bsdApplication;
+
+        public ReportController(IBsdApplicationService bsdApplication)
+        {
+            _bsdApplication = bsdApplication;
+        }
+
+        [HttpPost("generate")]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status204NoContent)]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
+        [ProducesResponseType(StatusCodes.Status500InternalServerError)]
+        public async Task<IActionResult> GenerateReport([FromBody] ReportRequest request)
+        {
+            if (!ModelState.IsValid)
+                return BadRequest(nameof(ModelState));
+
+            var result = await _bsdApplication.GenerateReportAsync(request);
+
+            if (result)
+                return Ok("Report generation initiated successfully.");
+            else
+                return StatusCode((int)HttpStatusCode.InternalServerError, "Failed to generate the report.");
+
+        }
+    }
+}
