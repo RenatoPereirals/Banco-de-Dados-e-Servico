09a40af970ca806511c6b351511da13d7f6f8005
renatoPreirals <renatopreirals@gmail.com>
2024-08-16 16:47:50 -0300

    feat: Add MarkResponse class for external API responses
    - Created `MarkResponse` class to handle responses from external API for marking.
    - Includes properties for employee ID, incorrect flag, year, month, day, hour, and minute.

---
+++ src/Bsd.Application/DTOs/MarkResponse.cs
@@ -0,0 +1,13 @@
+﻿namespace Bsd.Application.DTOs
+{
+    public class MarkResponse
+    {
+        public int Matricula { get; set; }
+        public bool Indevido { get; set; }
+        public int Ano { get; set; }
+        public int Mes { get; set; }
+        public int Dia { get; set; }
+        public int Hora { get; set; }
+        public int Minuto { get; set; }
+    }
+}
