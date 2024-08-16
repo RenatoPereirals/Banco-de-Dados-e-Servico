7cbc805d603e9e3665dff2ec47a059d94a53c544
renatoPreirals <renatopreirals@gmail.com>
2024-08-16 16:54:00 -0300

    feat: Enhance DateHelper with new date creation methods
    - Removed redundant exception handling code and simplified the `ParseDate` method.
    - Added `CreateDateTime` method for creating `DateTime` instances from year, month, day, hour, and minute components.
    - Added `CreateDateTimeFromMark` method to create a `DateTime` from `MarkResponse` properties.

--- src/Bsd.Application/Helpers/DateHelper.cs
+++ src/Bsd.Application/Helpers/DateHelper.cs
@@ -1,4 +1,5 @@
-using Bsd.Domain.Services.Interfaces;
+using Bsd.Application.DTOs;
+using Bsd.Application.Interfaces;
 
 using System.Globalization;
 
@@ -21,24 +22,33 @@ namespace Bsd.Application.Helpers
                 throw new ArgumentNullException(nameof(date), "The date field is required.");
             }
 
-            try
+            string dateDecoded = System.Net.WebUtility.UrlDecode(date);
+            string[] formats = { "dd/MM/yyyy", "MM/dd/yyyy", "d/M/yyyy", "yyyy-MM-dd" };
+            if (DateTime.TryParseExact(dateDecoded, formats, _culture, DateTimeStyles.None, out DateTime parsedDate))
             {
-                string dateDecoded = System.Net.WebUtility.UrlDecode(date);
-                string[] formats = { "dd/MM/yyyy", "MM/dd/yyyy","d/M/yyyy", "yyyy-MM-dd" };
-                if (DateTime.TryParseExact(dateDecoded, formats, _culture, DateTimeStyles.None, out DateTime parsedDate))
-                {
-                    return parsedDate;
-                }
-                else
-                {
-                    throw new FormatException("Invalid date format");
-                }
+                return parsedDate;
             }
-            catch (Exception ex)
+            else
             {
-                Console.WriteLine($"Failed to parse date: {ex.Message}");
-                throw new FormatException("Failed to parse date");
+                throw new FormatException("Invalid date format");
             }
+        }
+
+        public DateTime CreateDateTime(int year, int month, int day, int hour = 0, int minute = 0)
+        {
+            if (year < 1 || month < 1 || month > 12 || day < 1 || day > DateTime.DaysInMonth(year, month))
+                throw new ArgumentException("Invalid date components.");
+
+            if (hour < 0 || hour > 23 || minute < 0 || minute > 59)
+                throw new ArgumentException("Invalid time components.");
+
+            return new DateTime(year, month, day, hour, minute, 0);
+        }
+
+        public DateTime CreateDateTimeFromMark(MarkResponse mark)
+        {
+            return CreateDateTime(mark.Ano, mark.Mes, mark.Dia, mark.Hora, mark.Minuto);
         }
     }
 }
+
