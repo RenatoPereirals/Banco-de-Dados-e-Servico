478d67953eb341c942eb6353f92f2098b5d38900
renatoPreirals <renatopreirals@gmail.com>
2024-08-16 16:45:33 -0300

    feat: Add MarkRequest class for external API requests
    - Created `MarkRequest` class to handle API requests for marking with external services.
    - Includes properties for employee IDs, start and end dates, and response type.
    - Default response type set to "AS400V1".

---
+++ src/Bsd.Application/DTOs/MarkRequest.cs
@@ -0,0 +1,15 @@
+﻿namespace Bsd.Application.DTOs
+{
+    public class MarkRequest
+    {
+        public MarkRequest(List<int> employeeIds)
+        {
+            MatriculaPessoa = employeeIds;
+        }
+
+        public List<int> MatriculaPessoa { get; set; }
+        public string DataInicio { get; set; } = string.Empty;
+        public string DataFim { get; set; } = string.Empty;
+        public string ResponseType { get; set; } = "AS400V1";
+    }
+}
