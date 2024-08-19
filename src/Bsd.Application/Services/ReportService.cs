5491977a40c8f16ec710c85b44ebd4207b9f4dde
renatoPreirals <renatopreirals@gmail.com>
2024-08-19 09:06:12 -0300

    feat: Add ReportService to generate report files
    - Implemented the `ReportService` class with the `GenerateReport` method.
    - The method takes a collection of `ReportResponse` objects and an output file path, generating a report file in CSV format.
    - Ensured UTF-8 encoding for the generated report file.

---
+++ src/Bsd.Application/Services/ReportService.cs
@@ -0,0 +1,22 @@
+﻿using Bsd.Application.Interfaces;
+using Bsd.Application.DTOs;
+
+using System.Text;
+
+namespace Bsd.Application.Services
+{
+    public class ReportService : IReportService
+    {
+        public async Task<bool> GenerateReport(IEnumerable<ReportResponse> reportResponses, string outputPath)
+        {
+            if (reportResponses == null || !reportResponses.Any())
+                return false;
+
+            var lines = reportResponses.Select(r => $"{r.MatriculaPessoa};{r.Rubric};{r.TotalHours}");
+
+            await File.WriteAllLinesAsync(outputPath, lines, Encoding.UTF8);
+
+            return true;
+        }
+    }
+}
