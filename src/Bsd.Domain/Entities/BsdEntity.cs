e33d557ad64a7eb07aa0fb2e81e252b2c4e6a424
renatoPreirals <renatopreirals@gmail.com>
2024-08-19 09:07:43 -0300

    refactor: Update BsdEntity structure with start and end time properties
    - Removed `BsdId` and `EmployeeRubricAssignments` properties.
    - Added `StartTime` and `EndTime` properties to track service time intervals.
    - Initialized `Employees` collection with a `List<Employee>` instead of a `HashSet<Employee>`.
    - Simplified the class structure for better alignment with business logic requirements.

--- src/Bsd.Domain/Entities/BsdEntity.cs
+++ src/Bsd.Domain/Entities/BsdEntity.cs
@@ -1,21 +1,13 @@
 ﻿using Bsd.Domain.Enums;
 
-using System.ComponentModel.DataAnnotations;
-
 namespace Bsd.Domain.Entities
 {
     public class BsdEntity
     {
-        public BsdEntity()
-        {
-            Employees = new HashSet<Employee>();
-        }
-
-        [Key]
-        public int BsdId { get; set; }
         public DateTime DateService { get; set; }
         public DayType DayType { get; set; }
-        public ICollection<Employee> Employees { get; set; }
-        public List<EmployeeRubricAssignment> EmployeeRubricAssignments { get; set; } = new();
+        public TimeOnly StartTime { get; set; }
+        public TimeOnly EndTime { get; set; }
+        public ICollection<Employee> Employees { get; set; } = new List<Employee>();
     }
 }
