c5321714ca0bd821d69accfcbdca3951061ed0fd
renatoPreirals <renatopreirals@gmail.com>
2024-08-19 09:26:52 -0300

    refactor: Simplify BsdRepository by removing unused dependency
    - Removed dependency on IGeralRepository from BsdRepository constructor.
    - Updated `GetBsdEntitiesByDateRangeAsync` to return `ICollection<BsdEntity>` for consistency.
    - Simplified query methods and removed redundant ordering in `GetBsdByIdAsync`.
    - Refined `AddEmployeeToBsdAsync` to handle employees more efficiently.

--- src/Bsd.Infrastructure/RepositoryImpl/BsdRepository.cs
+++ src/Bsd.Infrastructure/RepositoryImpl/BsdRepository.cs
@@ -2,6 +2,7 @@ using Bsd.Domain.Entities;
 using Bsd.Domain.Repository.Interfaces;
 
 using Bsd.Infrastructure.Context;
+
 using Microsoft.EntityFrameworkCore;
 
 namespace Bsd.Infrastructure.RepositoryImpl
@@ -9,12 +10,9 @@ namespace Bsd.Infrastructure.RepositoryImpl
     public class BsdRepository : GeralRepository, IBsdRepository
     {
         private readonly BsdDbContext _context;
-        private readonly IGeralRepository _geralRepository;
-        public BsdRepository(BsdDbContext context,
-                             IGeralRepository geralRepository) : base(context)
+        public BsdRepository(BsdDbContext context) : base(context)
         {
             _context = context;
-            _geralRepository = geralRepository;
         }
 
         public async Task<bool> CreateBsdAsync(BsdEntity bsd)
@@ -23,7 +21,7 @@ namespace Bsd.Infrastructure.RepositoryImpl
             return (await _context.SaveChangesAsync()) > 0;
         }
 
-        public async Task<IEnumerable<BsdEntity>> GetBsdEntitiesByDateRangeAsync(DateTime startDate, DateTime endDate)
+        public async Task<ICollection<BsdEntity>> GetBsdEntitiesByDateRangeAsync(DateTime startDate, DateTime endDate)
         {
             return await GetBsdQuery()
                .Where(b => b.DateService.Date >= startDate.Date && b.DateService.Date <= endDate.Date)
@@ -33,14 +31,6 @@ namespace Bsd.Infrastructure.RepositoryImpl
         public async Task<IEnumerable<BsdEntity>> GetAllBsdAsync()
         {
             return await GetBsdQuery().ToListAsync();
-        }
-
-        public async Task<BsdEntity> GetBsdByIdAsync(int bsdId)
-        {
-            return await GetBsdQuery()
-                .OrderBy(b => b.BsdId)
-                .FirstOrDefaultAsync(b => b.BsdId == bsdId)
-                ?? throw new InvalidOperationException($"Bsd com o número {bsdId} não encontrado.");
         }
 
         private IQueryable<BsdEntity> GetBsdQuery()
