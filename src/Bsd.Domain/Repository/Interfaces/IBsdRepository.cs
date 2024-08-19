ad57a5cdb69883a0472b2019479a93101faa2a2a
renatoPreirals <renatopreirals@gmail.com>
2024-08-19 09:10:53 -0300

    refactor: Remove GetBsdByIdAsync method from IBsdRepository
    - Removed the `GetBsdByIdAsync` method from the `IBsdRepository` interface.
    - Updated the interface to focus on retrieving BSD entities by date range and managing employee associations.

--- src/Bsd.Domain/Repository/Interfaces/IBsdRepository.cs
+++ src/Bsd.Domain/Repository/Interfaces/IBsdRepository.cs
@@ -4,9 +4,8 @@ namespace Bsd.Domain.Repository.Interfaces
 {
     public interface IBsdRepository
     {
-        Task<BsdEntity> GetBsdByIdAsync(int bsdId);
         Task<IEnumerable<BsdEntity>> GetAllBsdAsync();
-        Task<IEnumerable<BsdEntity>> GetBsdEntitiesByDateRangeAsync(DateTime startDate, DateTime endDate);
+        Task<ICollection<BsdEntity>> GetBsdEntitiesByDateRangeAsync(DateTime startDate, DateTime endDate);
         Task<ICollection<Employee>> AddEmployeeToBsdAsync(BsdEntity bsd);
         Task<bool> CreateBsdAsync(BsdEntity bsd);
     }
