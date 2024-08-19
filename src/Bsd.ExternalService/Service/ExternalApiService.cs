f9fb820248c60bc43d76acba22e8986bd7f56d81
renatoPreirals <renatopreirals@gmail.com>
2024-08-19 09:25:23 -0300

    feat: Implement ExternalApiService for handling API requests
    - Added `ExternalApiService` class to interact with external APIs.
    - Implements `IExternalApiService` interface.
    - Utilizes `HttpClient` for making POST requests to fetch marks based on `MarkRequest`.
    - Includes request serialization, setting headers, and response handling with error checking.
    - Deserializes API responses and throws exceptions for unsuccessful requests.

---
+++ src/Bsd.ExternalService/Service/ExternalApiService.cs
@@ -0,0 +1,51 @@
+﻿using System.Text.Json;
+using System.Text;
+
+using Bsd.Application.Interfaces;
+using Bsd.Application.DTOs;
+using Bsd.Application.Utilities;
+using Bsd.ExternalService_.Models;
+
+namespace Bsd.ExternalService.Service
+{
+    public class ExternalApiService : IExternalApiService
+    {
+        private readonly HttpClient _httpClient;
+        private readonly string _urlApi;
+        private readonly string _identifier;
+        private readonly string _token;
+
+        public ExternalApiService()
+        {
+            _httpClient = new HttpClient();
+            _urlApi = AppSettings.ApiUrl;
+            _identifier = AppSettings.Identifier;
+            _token = AppSettings.Token;
+
+        }
+
+        public async Task<ICollection<MarkResponse>> GetMarkAsync(MarkRequest request)
+        {
+            string requestJson = JsonSerializer.Serialize(request);
+            var content = new StringContent(requestJson, Encoding.UTF8, "application/json");
+            content.Headers.Add("identifier", _identifier);
+            content.Headers.Add("key", _token);
+
+            HttpResponseMessage response = await _httpClient.PostAsync($"{_urlApi}/RestServiceApi/Mark/GetMarks", content);
+            response.EnsureSuccessStatusCode();
+
+            var responseJson = await response.Content.ReadAsStringAsync();
+            var apiResponse = JsonSerializer.Deserialize<ApiResponse<List<MarkResponse>>>(responseJson);
+
+            if (apiResponse!.Sucesso)
+            {
+                return apiResponse.Obj;
+            }
+            else
+            {
+                throw new InvalidOperationException($"Erro na requisição: {apiResponse.Mensagem}");
+            }
+        }
+
+    }
+}
