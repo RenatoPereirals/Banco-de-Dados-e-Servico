62e563210e14768e552d590c0f3c7b9ae2b548ff
renatoPreirals <renatopreirals@gmail.com>
2024-08-20 16:47:55 -0300

    refactor: Update MarkService constructor parameters order
    The MarkService constructor parameters have been reordered to match the order of dependencies in the constructor implementation. This change improves code readability and consistency.

--- src/Bsd.Application/Services/MarkService.cs
+++ src/Bsd.Application/Services/MarkService.cs
@@ -1,41 +1,36 @@
 using Bsd.Application.Interfaces;
-
 using Bsd.Application.DTOs;
 
-using Bsd.Domain.Services.Interfaces;
 using Bsd.Domain.Entities;
+using Bsd.Domain.Services.Interfaces;
 
 namespace Bsd.Application.Services
 {
     public class MarkService : IMarkService
     {
-        private readonly IBsdService _bsdService;
-        private readonly IEmployeeService _employeeService;
         private readonly IMarkProcessor _mappingMark;
+        private readonly IEmployeeService _employeeService;
 
-        public MarkService(IBsdService bsdService,
-                           IEmployeeService employeeService,
-                           IMarkProcessor mappingMark)
+        public MarkService(IMarkProcessor mappingMark, IEmployeeService employeeService)
         {
-            _bsdService = bsdService;
-            _employeeService = employeeService;
             _mappingMark = mappingMark;
+            _employeeService = employeeService;
         }
 
-        public async Task<ICollection<BsdEntity>> ProcessMarksAsync(ICollection<MarkResponse> markResponses)
+        public async Task<BsdEntity> ProcessMarksAsync(ICollection<MarkResponse> markResponses)
         {
             if (markResponses == null || !markResponses.Any())
+            {
                 throw new ArgumentException("Mark responses cannot be null or empty.", nameof(markResponses));
+            }
 
-            var bsdEntities = _mappingMark.ProcessMarks(markResponses);
+            var employees = _mappingMark.ProcessMarks(markResponses); // aqui a data de inicio e fim dos dias trabalhados e as horas de entrada e saída estão sendo associadas corretamente
 
             var employeeIds = markResponses.Select(mark => mark.Matricula).ToList();
+            var bsdEntity = await _employeeService.AssociateEmployeesToBsdAsync(employees)
+                ?? throw new InvalidOperationException("Failed to process BSD.");
 
-            await _employeeService.AssociateEmployeesToBsdAsync(bsdEntities, employeeIds);
-
-            var processedBsdEntities = await _bsdService.CreateOrUpdateBsdsAsync(bsdEntities);
-
-            return processedBsdEntities ??
+            return bsdEntity ??
                 throw new InvalidOperationException("Failed to process BSD.");
         }
     }
