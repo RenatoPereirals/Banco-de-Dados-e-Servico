52e5fcbad238eded8a8e07adba1540aad38ffeda
renatoPreirals <renatopreirals@gmail.com>
2024-08-20 16:49:11 -0300

    refactor: Reorder constructor parameters in BsdService
    The constructor parameters in the BsdService class have been reordered to match the order of dependencies in the constructor implementation. This change improves code readability and consistency.

--- src/Bsd.Domain/Services/BsdService.cs
+++ src/Bsd.Domain/Services/BsdService.cs
@@ -5,42 +5,19 @@ namespace Bsd.Domain.Services
 {
     public class BsdService : IBsdService
     {
-        private readonly IDayTypeChecker _dayTypeChecker;
-        private readonly IRubricService _rubricService;
         private readonly ICalculateRubricHours _calculateRubricHours;
+        private readonly IRubricService _rubricService;
 
-        public BsdService(IDayTypeChecker dayTypeChecker,
-                          IRubricService rubricService,
-                          ICalculateRubricHours calculateRubricsHours)
+        public BsdService(ICalculateRubricHours calculateRubricHours, IRubricService rubricService)
         {
-            _dayTypeChecker = dayTypeChecker;
+            _calculateRubricHours = calculateRubricHours;
             _rubricService = rubricService;
-            _calculateRubricHours = calculateRubricsHours;
         }
 
-        public async Task<BsdEntity> CreateBsdAsync(BsdEntity bsd)
+        public async Task CreateOrUpdateBsdsAsync(BsdEntity bsd)
         {
-            var day = await _dayTypeChecker.GetDayType(bsd.DateService); // verificar se esse método é redundante
-
-            bsd.DayType = day;
             await _rubricService.AssociateRubricAsync(bsd);
-
-            return bsd;
-        }
-
-        public async Task<ICollection<BsdEntity>> CreateOrUpdateBsdsAsync(ICollection<BsdEntity> bsds)
-        {
-            var responses = new List<BsdEntity>();
-
-            _calculateRubricHours.CalculateTotalWorkedHours(bsds);
-
-            foreach (var bsd in bsds)
-            {
-                await CreateBsdAsync(bsd);
-                responses.Add(bsd);
-            }
-
-            return responses;
+            _calculateRubricHours.CalculateTotalWorkedHours(bsd);
         }
     }
 }
