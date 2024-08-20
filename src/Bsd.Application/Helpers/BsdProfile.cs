5eef530076c2f84521ccb6b4349de82270d9ee38
renatoPreirals <renatopreirals@gmail.com>
2024-08-20 16:45:52 -0300

    refactor: Remove unused BsdProfile mappings and simplify code

--- src/Bsd.Application/Helpers/BsdProfile.cs
+++ src/Bsd.Application/Helpers/BsdProfile.cs
@@ -9,16 +9,7 @@ namespace Bsd.Application.Helpers
     {
         public BsdProfile()
         {
-            CreateMap<CreateBsdRequest, BsdEntity>()
-                .ForMember(dest => dest.DateService, opt => opt.MapFrom(src => src.DateServiceDate))
-                .ReverseMap();
-
             CreateMap<MarkResponse, BsdEntity>()
-                .ForMember(dest => dest.DateService, opt => opt.MapFrom<DateHelperValueResolver>())
-                .ForMember(dest => dest.StartTime, opt => opt.MapFrom(src => new TimeOnly(src.Hora, src.Minuto)))
-                .ForMember(dest => dest.EndTime, opt => opt.MapFrom(src => new TimeOnly(src.Hora, src.Minuto)))
-                .ForMember(dest => dest.DayType, opt => opt.Ignore())
-                .ForMember(dest => dest.Employees, opt => opt.Ignore())
                 .ReverseMap();
 
             CreateMap<BsdEntity, List<ReportResponse>>()
