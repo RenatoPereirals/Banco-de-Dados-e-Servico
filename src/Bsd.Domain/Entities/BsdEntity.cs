e69c33991b8367b2ff9f3cd4e4b2f097ddd00f8e
renatoPreirals <renatopreirals@gmail.com>
2024-08-20 16:48:15 -0300

    refactor: Remove unused properties and dependencies in BsdEntity

--- src/Bsd.Domain/Entities/BsdEntity.cs
+++ src/Bsd.Domain/Entities/BsdEntity.cs
@@ -1,13 +1,7 @@
-﻿using Bsd.Domain.Enums;
-
-namespace Bsd.Domain.Entities
+﻿namespace Bsd.Domain.Entities
 {
     public class BsdEntity
     {
-        public DateTime DateService { get; set; }
-        public DayType DayType { get; set; }
-        public TimeOnly StartTime { get; set; }
-        public TimeOnly EndTime { get; set; }
         public ICollection<Employee> Employees { get; set; } = new List<Employee>();
     }
 }
