3894c85cc4fb047d37afae5b00c28999b0b136c9
renatoPreirals <renatopreirals@gmail.com>
2024-09-06 09:35:42 -0300

    refactor: enhance MarkProcessor with improved methods and dependency injection
    - Introduced IStaticDataService dependency to fetch employee data, improving data consistency.
    - Refactored mark processing logic into distinct methods for better readability and maintainability.
      - Added GroupMarksByEmployee method to group and sort marks by employee.
      - Created AssignWorkedDaysToEmployee method to assign worked days to an employee.
      - Improved GetWorkedDays method to handle continuous and non-continuous marks more effectively.
    - Added logging for mark processing to assist with debugging and monitoring.
    - Updated CreateWorkedDay method to return a worked day with start and end times.

--- src/Bsd.Application/Services/MarkProcessor.cs
+++ src/Bsd.Application/Services/MarkProcessor.cs
@@ -1,73 +1,111 @@
 using Bsd.Application.DTOs;
 using Bsd.Application.Interfaces;
+
 using Bsd.Domain.Entities;
+using Bsd.Domain.Services.Interfaces;
 
 namespace Bsd.Application.Services
 {
     public class MarkProcessor : IMarkProcessor
     {
+        private readonly IStaticDataService _staticDataService;
+
+        public MarkProcessor(IStaticDataService staticDataService)
+        {
+            _staticDataService = staticDataService;
+        }
+
         public ICollection<Employee> ProcessMarks(ICollection<MarkResponse> marks)
         {
-            // Agrupa as marcações por funcionário
-            var groupedMarks = marks
-                .GroupBy(m => m.Matricula)
-                .Select(g => g.OrderBy(m => new DateTime(m.Ano, m.Mes, m.Dia, m.Hora, m.Minuto, 0)).ToList())
-                .ToList();
-
+            var groupedMarks = GroupMarksByEmployee(marks);
             var employees = new List<Employee>();
 
             foreach (var employeeMarks in groupedMarks)
             {
-                var employee = new Employee
+                var employee = AssignWorkedDaysToEmployee(employeeMarks);
+                if (employee != null)
                 {
-                    EmployeeId = employeeMarks.First().Matricula,
-                    WorkedDays = new List<WorkedDay>()
-                };
-
-                var startMark = employeeMarks.OrderBy(m => new DateTime(m.Ano, m.Mes, m.Dia, m.Hora, m.Minuto, 0)).First();
-                var endMark = startMark;
-
-                for (int i = 1; i < employeeMarks.Count; i++)
-                {
-                    var currentMark = employeeMarks[i];
-                    var currentDate = new DateTime(currentMark.Ano, currentMark.Mes, currentMark.Dia);
-                    var endDate = new DateTime(endMark.Ano, endMark.Mes, endMark.Dia);
-
-                    // Se as datas forem consecutivas, atualiza o endMark
-                    if (currentDate.Date == endDate.Date.AddDays(1))
-                    {
-                        endMark = currentMark;
-                    }
-                    else
-                    {
-                        // Adiciona um WorkedDay ao Employee
-                        employee.WorkedDays.Add(CreateWorkedDay(startMark, endMark));
-
-                        // Reinicia o startMark e endMark para o próximo grupo de dias consecutivos
-                        startMark = currentMark;
-                        endMark = currentMark;
-                    }
+                    employees.Add(employee);
                 }
-
-                // Adiciona o último WorkedDay do funcionário
-                employee.WorkedDays.Add(CreateWorkedDay(startMark, endMark));
-
-                // Adiciona o Employee à lista de funcionários
-                employees.Add(employee);
             }
 
             return employees;
         }
 
+
+        private List<List<MarkResponse>> GroupMarksByEmployee(ICollection<MarkResponse> marks)
+        {
+            var grouped = marks
+                .GroupBy(m => m.Matricula)
+                .Select(g => g.OrderBy(m => new DateTime(m.Ano, m.Mes, m.Dia, m.Hora, m.Minuto, 0)).ToList())
+                .ToList();
+
+            return grouped;
+        }
+
+
+        private Employee AssignWorkedDaysToEmployee(List<MarkResponse> employeeMarks)
+        {
+            var employees = _staticDataService.GetEmployees();
+            var employee = employees.FirstOrDefault(e => e.EmployeeId == employeeMarks.First().Matricula) ?? throw new Exception("Employee not found");
+            var workedDays = GetWorkedDays(employeeMarks);
+
+            foreach (var workedDay in workedDays)
+            {
+                employee.WorkedDays.Add(workedDay);
+            }
+
+            return employee;
+        }
+
+        private List<WorkedDay> GetWorkedDays(List<MarkResponse> employeeMarks)
+        {
+            var workedDays = new List<WorkedDay>();
+
+            List<MarkResponse> sortedMarks = employeeMarks.OrderBy(m => new DateTime(m.Ano, m.Mes, m.Dia, m.Hora, m.Minuto, 0)).ToList();
+            MarkResponse startMark = sortedMarks.First();
+            MarkResponse endMark = startMark;
+
+            bool hasContinuousMarks = false;
+
+            Console.WriteLine($"Processando {sortedMarks.Count} marca��es para o funcion�rio {startMark.Matricula}...");
+
+            for (int i = 1; i < sortedMarks.Count; i++)
+            {
+                var currentMark = sortedMarks[i];
+                var currentDate = new DateTime(currentMark.Ano, currentMark.Mes, currentMark.Dia);
+                var endDate = new DateTime(endMark.Ano, endMark.Mes, endMark.Dia);
+
+                if (currentDate.Date == endDate.Date.AddDays(1))
+                {
+                    endMark = currentMark;
+                    workedDays.Add(CreateWorkedDay(startMark, endMark));
+                    hasContinuousMarks = true;
+                }
+                else
+                {
+                    startMark = currentMark;
+                    endMark = currentMark;
+                }
+            }
+
+            if (!hasContinuousMarks)
+                workedDays.Clear();
+
+            return workedDays;
+        }
+
         private WorkedDay CreateWorkedDay(MarkResponse startMark, MarkResponse endMark)
         {
-            return new WorkedDay
+            var workedDay = new WorkedDay
             {
                 DateEntry = new DateTime(startMark.Ano, startMark.Mes, startMark.Dia),
                 DateExit = new DateTime(endMark.Ano, endMark.Mes, endMark.Dia),
                 StartTime = new TimeOnly(startMark.Hora, startMark.Minuto),
                 EndTime = new TimeOnly(endMark.Hora, endMark.Minuto)
             };
+
+            return workedDay;
         }
     }
 }
