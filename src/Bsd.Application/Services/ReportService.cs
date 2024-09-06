d7a8339a8eb8cdb84d192c0b28ddabe7ea96bf2f
renatoPreirals <renatopreirals@gmail.com>
2024-09-06 09:37:34 -0300

    refactor: enhance ReportService with additional dependencies and report processing logic
    - Added dependencies for IMarkService, IBsdApplicationService, IStaticDataService, and IDateHelper to improve report generation and processing.
    - Introduced ProcessReportAsync method to handle end-to-end report processing, including fetching marks, processing employees, and creating the report.
    - Refactored GenerateOutputPath method to ensure directory creation and file path generation.
    - Added CreateReportAsync method to generate report responses from BsdEntity and format them for output.
    - Enhanced GenerateReportAsync method to handle formatting and writing report data with improved line separation for different employees.
    - Added validation and error handling for report generation steps.

--- src/Bsd.Application/Services/ReportService.cs
+++ src/Bsd.Application/Services/ReportService.cs
@@ -1,39 +1,116 @@
 ﻿using Bsd.Application.Interfaces;
 using Bsd.Application.DTOs;
 
+using Bsd.Domain.Services.Interfaces;
+using Bsd.Domain.Entities;
+
 using System.Text;
-using System.Security.Cryptography.X509Certificates;
 
-namespace Bsd.Application.Services
+namespace Bsd.Application.Services;
+
+public class ReportService : IReportService
 {
-    public class ReportService : IReportService
+    private readonly IMarkService _markService;
+    private readonly IBsdApplicationService _bsdApplicationService;
+    private readonly IStaticDataService _staticDataService;
+    private readonly IDateHelper _dateHelper;
+
+    public ReportService(IMarkService markService,
+                         IBsdApplicationService bsdApplicationService,
+                        IStaticDataService staticDataService,
+                        IDateHelper dateHalper)
     {
-        public async Task<bool> GenerateReport(IEnumerable<ReportResponse> reportResponses, string outputPath)
-        {
-            if (reportResponses == null || !reportResponses.Any())
-                return false;
-
-            var lines = reportResponses.Select(r => $"{r.MatriculaPessoa};{r.Rubric};{r.TotalHours}");
-
-            await File.WriteAllLinesAsync(outputPath, lines, Encoding.UTF8);
-
-            return true;
-        }
-
-        public string GenerateOutputPath()
-        {
-            var reportDirectory = Path.Combine(Directory.GetCurrentDirectory(), "Report");
-
-            if (!Directory.Exists(reportDirectory))
-            {
-                Directory.CreateDirectory(reportDirectory);
-            }
-
-            var fileName = $"Report_{DateTime.Now:yyyyMMdd_HHmmss}.txt";
-
-            var outputPath = Path.Combine(reportDirectory, fileName);
-
-            return outputPath;
-        }
+        _markService = markService;
+        _bsdApplicationService = bsdApplicationService;
+        _staticDataService = staticDataService;
+        _dateHelper = dateHalper;
     }
+
+    public async Task<bool> ProcessReportAsync(ReportRequest request)
+    {
+        var employees = _staticDataService.GetEmployees();
+
+        var parseDate = _dateHelper.ParseDate(request.DataFim).AddDays(1);
+        request.DataFim = _dateHelper.ParseString(parseDate);
+
+        var marks = await _markService.GetMarksForEmployeesAsync(employees, request);
+
+        ICollection<Employee> processedEmployees = _markService.ProcessMarksAsync(marks);
+
+        var bsdEntity = await _bsdApplicationService.CreatebsdEntityAsync(processedEmployees);
+
+        var outputPath = GenerateOutputPath();
+        bool reportGenerated = await CreateReportAsync(bsdEntity, outputPath);
+
+        if (!reportGenerated)
+            throw new Exception("Error generating report");
+
+        return reportGenerated;
+    }
+
+    private static string GenerateOutputPath()
+    {
+        var reportDirectory = Path.Combine(Directory.GetCurrentDirectory(), "Report");
+
+        if (!Directory.Exists(reportDirectory))
+            Directory.CreateDirectory(reportDirectory);
+
+        var fileName = $"Report_{DateTime.Now:yyyyMMdd_HHmmss}.txt";
+        return Path.Combine(reportDirectory, fileName);
+    }
+
+
+    private async Task<bool> CreateReportAsync(BsdEntity bsdEntity, string outputPath)
+    {
+        var reportResponses = new List<ReportResponse>();
+
+
+        foreach (var employee in bsdEntity.Employees)
+        {
+            var employeeRubrics = employee.Rubrics
+                .GroupBy(r => r.RubricId)
+                .Select(g => new ReportResponse
+                {
+                    MatriculaPessoa = employee.EmployeeId,
+                    Rubric = g.Key,
+                    TotalHours = g.First().TotalWorkedHours
+                })
+                .ToList();
+
+            reportResponses.AddRange(employeeRubrics);
+        }
+
+        reportResponses = reportResponses
+            .OrderBy(r => r.MatriculaPessoa)
+            .ThenBy(r => r.Rubric)
+            .ToList();
+
+        return await GenerateReportAsync(reportResponses, outputPath);
+    }
+
+    private async Task<bool> GenerateReportAsync(IEnumerable<ReportResponse> reportResponses, string outputPath)
+    {
+        if (reportResponses == null || !reportResponses.Any())
+            return false;
+
+        var lines = new List<string>();
+        int? previousMatricula = null;
+
+        foreach (var response in reportResponses)
+        {
+            if (previousMatricula != null && previousMatricula != response.MatriculaPessoa)
+                lines.Add(string.Empty);
+
+            lines.Add($"{response.MatriculaPessoa:D8};{response.Rubric:D5};{response.TotalHours}");
+
+            previousMatricula = response.MatriculaPessoa;
+        }
+
+        await File.WriteAllLinesAsync(outputPath, lines, Encoding.UTF8);
+
+        return true;
+    }
+
+
+
 }
