97f3770cf6a12f2ea0dda3acc73ba52dc7bba19b
renatoPreirals <renatopreirals@gmail.com>
2024-09-06 09:39:18 -0300

    refactor: enhance BsdService with duplication check and improved rubric association
    - Updated CreateOrUpdateBsdsAsync method to call AssociateRubricsToEmployeeAsync instead of AssociateRubricAsync for better rubric association handling.
    - Added logic to check for duplicated rubrics within each employee's rubric list, logging a message if duplicates are found.
    - Refactored the rubric association and duplication check process for improved robustness and clarity.
    - Added console logging to identify and track duplicate rubrics for better debugging and monitoring.

--- src/Bsd.Domain/Services/BsdService.cs
+++ src/Bsd.Domain/Services/BsdService.cs
@@ -1,23 +1,34 @@
 using Bsd.Domain.Services.Interfaces;
 using Bsd.Domain.Entities;
 
-namespace Bsd.Domain.Services
+namespace Bsd.Domain.Services;
+
+public class BsdService : IBsdService
 {
-    public class BsdService : IBsdService
+    private readonly ICalculateRubricHours _calculateRubricHours;
+    private readonly IRubricService _rubricService;
+
+    public BsdService(ICalculateRubricHours calculateRubricHours, IRubricService rubricService)
     {
-        private readonly ICalculateRubricHours _calculateRubricHours;
-        private readonly IRubricService _rubricService;
+        _calculateRubricHours = calculateRubricHours;
+        _rubricService = rubricService;
+    }
 
-        public BsdService(ICalculateRubricHours calculateRubricHours, IRubricService rubricService)
+    public async Task CreateOrUpdateBsdsAsync(BsdEntity bsd)
+    {
+        await _rubricService.AssociateRubricsToEmployeeAsync(bsd);
+
+        foreach (var employee in bsd.Employees)
         {
-            _calculateRubricHours = calculateRubricHours;
-            _rubricService = rubricService;
+            var duplicatedRubrics = employee.Rubrics
+                .GroupBy(r => r.RubricId)
+                .Where(g => g.Count() > 1)
+                .ToList();
+
+            if (duplicatedRubrics.Any())
+                Console.WriteLine($"Duplicated rubrics found for EmployeeId: {employee.EmployeeId}");
         }
 
-        public async Task CreateOrUpdateBsdsAsync(BsdEntity bsd)
-        {
-            await _rubricService.AssociateRubricAsync(bsd);
-            _calculateRubricHours.CalculateTotalWorkedHours(bsd);
-        }
+        _calculateRubricHours.CalculateTotalWorkedHours(bsd);
     }
 }
