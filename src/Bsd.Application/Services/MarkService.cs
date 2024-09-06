f93e38854951863f0f6d9241885080c73c1e2864
renatoPreirals <renatopreirals@gmail.com>
2024-09-06 09:36:52 -0300

    refactor: update MarkService to include additional dependencies and methods
    - Added IExternalApiService dependency for fetching mark data from external APIs.
    - Introduced GetMarksForEmployeesAsync method to retrieve marks based on employee data and report request.
    - Refactored ProcessMarksAsync method to handle mark processing and return a collection of employees.
    - Improved error handling to ensure mark responses are not null or empty and handle processing failures.
    - Removed redundant null checks and exception handling for a more streamlined implementation.

--- src/Bsd.Application/Services/MarkService.cs
+++ src/Bsd.Application/Services/MarkService.cs
@@ -10,27 +10,37 @@ namespace Bsd.Application.Services
     {
         private readonly IMarkProcessor _mappingMark;
         private readonly IEmployeeService _employeeService;
+        private readonly IExternalApiService _externalApiService;
 
-        public MarkService(IMarkProcessor mappingMark, IEmployeeService employeeService)
+        public MarkService(IMarkProcessor mappingMark,
+                           IEmployeeService employeeService,
+                           IExternalApiService externalApiService)
         {
             _mappingMark = mappingMark;
             _employeeService = employeeService;
+            _externalApiService = externalApiService;
         }
 
-        public async Task<BsdEntity> ProcessMarksAsync(ICollection<MarkResponse> markResponses)
+        public async Task<ICollection<MarkResponse>> GetMarksForEmployeesAsync(IEnumerable<Employee> employees, ReportRequest request)
+        {
+            var employeeIds = employees.Select(e => e.EmployeeId).ToList();
+            var markRequest = new MarkRequest(employeeIds)
+            {
+                DataInicio = request.DataInicio,
+                DataFim = request.DataFim
+            };
+
+            return await _externalApiService.GetMarkAsync(markRequest);
+        }
+
+        public ICollection<Employee> ProcessMarksAsync(ICollection<MarkResponse> markResponses)
         {
             if (markResponses == null || !markResponses.Any())
-            {
-                throw new ArgumentException("Mark responses cannot be null or empty.", nameof(markResponses));
-            }
+                throw new ArgumentException("Mark responses cannot be null or empty.", nameof(markResponses));            
 
-            var employees = _mappingMark.ProcessMarks(markResponses); // aqui a data de inicio e fim dos dias trabalhados e as horas de entrada e saída estão sendo associadas corretamente
+            var employees = _mappingMark.ProcessMarks(markResponses);
 
-            var employeeIds = markResponses.Select(mark => mark.Matricula).ToList();
-            var bsdEntity = await _employeeService.AssociateEmployeesToBsdAsync(employees)
-                ?? throw new InvalidOperationException("Failed to process BSD.");
-
-            return bsdEntity ??
+            return employees ??
                 throw new InvalidOperationException("Failed to process BSD.");
         }
     }
