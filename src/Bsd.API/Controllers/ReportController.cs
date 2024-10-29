0428b85a9e76f78ff0016bc4068258fee3d32d5b
renatoPreirals <renatopreirals@gmail.com>
2024-10-29 16:10:51 -0300

    feat: expand ReportController with employee filtering and additional endpoint
    - **ReportController**: Added `GenerateReportById` endpoint to allow report generation for specific employees based on `EmployeeId`.
    - Integrated `IStaticDataService` to retrieve and filter employees for targeted report processing.
    - **Error Handling**: Improved error handling for employee not found scenario, ensuring meaningful feedback.
    - Maintained existing `GenerateReport` endpoint with updated structure for consistency.

--- src/Bsd.API/Controllers/ReportController.cs
+++ src/Bsd.API/Controllers/ReportController.cs
@@ -4,6 +4,8 @@ using Bsd.Application.DTOs;
 using System.Net;
 
 using Microsoft.AspNetCore.Mvc;
+using Bsd.Domain.Services.Interfaces;
+using Bsd.Domain.Entities;
 
 namespace Bsd.API.Controllers
 {
@@ -12,10 +14,30 @@ namespace Bsd.API.Controllers
     public class ReportController : Controller
     {
         private readonly IReportService _reportService;
+        private readonly IStaticDataService _staticDataService;
 
-        public ReportController(IReportService reportService)
+        public ReportController(IReportService reportService,
+                                 IStaticDataService staticData)
         {
             _reportService = reportService;
+            _staticDataService = staticData;
+        }
+
+        [HttpPost("{id}")]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(typeof(string), StatusCodes.Status500InternalServerError)]
+        [ProducesDefaultResponseType]
+        public async Task<IActionResult> GenerateReportById([FromBody] ReportRequest request, int id)
+        {
+            IEnumerable<Employee> emplyess = _staticDataService.GetEmployees().Where(e => e.EmployeeId == id)
+                ?? throw new Exception("Employees not found");
+
+            var result = await _reportService.ProcessReportByIdAsync(request, emplyess);
+
+            if (result)
+                return Ok("Report generation initiated successfully.");
+            else
+                return StatusCode((int)HttpStatusCode.InternalServerError, "Failed to generate the report.");
         }
 
         [HttpPost()]
