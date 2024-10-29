e808945cbb627354001a8c65096831173be08839
renatoPreirals <renatopreirals@gmail.com>
2024-10-29 16:11:54 -0300

    feat: extend IReportService with ProcessReportByIdAsync method
    - **IReportService**: Added `ProcessReportByIdAsync` method to support report generation for specific employees.
    - Enhanced interface functionality to enable targeted processing by passing an `IEnumerable<Employee>` parameter.
    - Maintained existing `ProcessReportAsync` method to ensure compatibility with general report processing requirements.

--- src/Bsd.Application/Interfaces/IReportService.cs
+++ src/Bsd.Application/Interfaces/IReportService.cs
@@ -1,9 +1,11 @@
 using Bsd.Application.DTOs;
+using Bsd.Domain.Entities;
 
 namespace Bsd.Application.Interfaces
 {
     public interface IReportService
     {           
         Task<bool> ProcessReportAsync(ReportRequest request);
+        Task<bool> ProcessReportByIdAsync(ReportRequest request, IEnumerable<Employee> employees);
     }
 }
