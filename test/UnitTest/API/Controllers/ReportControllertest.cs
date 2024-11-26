e2fc96793984ad3c471e562519c89c37067bb5f0
renatoPreirals <renatopreirals@gmail.com>
2024-11-26 10:12:21 -0300

    test: add unit tests for ReportController's GenerateReport method
    Added tests for the `GenerateReport` method in the `ReportController`:
    - `GenerateReport_ReturnsOkResult_WhenReportGenerationIsSuccessful`: Verifies that the method returns a 200 OK result when report generation is successful.
    - `GenerateReport_ReturnsInternalServerError_WhenReportGenerationFails`: Ensures the method returns a 500 Internal Server Error when report generation fails.

---
+++ test/UnitTest/API/Controllers/ReportControllertest.cs
@@ -0,0 +1,62 @@
+using Bsd.Application.Interfaces;
+using Bsd.Application.DTOs;
+
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+using Moq;
+using Bsd.Domain.Services.Interfaces;
+
+namespace Bsd.API.Controllers.Tests
+{
+    public class ReportControllerTests
+    {
+        private readonly Mock<IReportService> _mockReportService;
+        private readonly ReportController _reportController;
+
+        public ReportControllerTests()
+        {
+            _mockReportService = new Mock<IReportService>();
+            var mockStaticDataService = new Mock<IStaticDataService>();
+            _reportController = new ReportController(_mockReportService.Object, mockStaticDataService.Object);
+        }
+
+        [Fact]
+        public async Task GenerateReport_ReturnsOkResult_WhenReportGenerationIsSuccessful()
+        {
+            // Arrange
+            var request = new ReportRequest();
+            _mockReportService
+                .Setup(rs => rs.ProcessReportAsync(request))
+                .ReturnsAsync(true);
+
+            // Act
+            var result = await _reportController.GenerateReport(request);
+
+            // Assert
+            Assert.IsType<OkObjectResult>(result);
+            var okResult = (OkObjectResult)result;
+            Assert.Equal(StatusCodes.Status200OK, okResult.StatusCode);
+            Assert.Equal("Report generation initiated successfully.", okResult.Value);
+        }
+
+        [Fact]
+        public async Task GenerateReport_ReturnsInternalServerError_WhenReportGenerationFails()
+        {
+            // Arrange
+            var request = new ReportRequest();
+            _mockReportService
+                .Setup(rs => rs.ProcessReportAsync(request))
+                .ReturnsAsync(false);
+
+            // Act
+            var result = await _reportController.GenerateReport(request);
+
+            // Assert
+            Assert.IsType<ObjectResult>(result);
+            var objectResult = (ObjectResult)result;
+            Assert.Equal(StatusCodes.Status500InternalServerError, objectResult.StatusCode);
+            Assert.Equal("Failed to generate the report.", objectResult.Value);
+        }
+    }
+}
