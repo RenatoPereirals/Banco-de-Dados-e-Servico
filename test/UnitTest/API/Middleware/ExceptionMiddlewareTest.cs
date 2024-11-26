3a3bba5ec01e92a8efec7b01f7e13cd1d1da6822
renatoPreirals <renatopreirals@gmail.com>
2024-11-26 10:13:52 -0300

    test(middleware): add unit test for ExceptionMiddleware's InvokeAsync method
    - Added `InvokeAsync_ShouldCallNextDelegate` to verify that the middleware invokes the next delegate in the pipeline.
    - Mocked `RequestDelegate` and `HttpContext` to simulate the pipeline behavior.

---
+++ test/UnitTest/API/Middleware/ExceptionMiddlewareTest.cs
@@ -0,0 +1,36 @@
+using Bsd.API.Middlewares;
+
+using Microsoft.AspNetCore.Http;
+
+using Moq;
+
+
+namespace Bsd.API.Tests.Middlewares
+{
+    public class ExceptionMiddlewareTest
+    {
+        private readonly Mock<RequestDelegate> _mockNext;
+        private readonly Mock<HttpContext> _mockHttpContext;
+        private readonly ExceptionMiddleware _exceptionMiddleware;
+
+        public ExceptionMiddlewareTest()
+        {
+            _mockNext = new Mock<RequestDelegate>();
+            _mockHttpContext = new Mock<HttpContext>();
+            _exceptionMiddleware = new ExceptionMiddleware(_mockNext.Object);
+        }
+
+        [Fact]
+        public async Task InvokeAsync_ShouldCallNextDelegate()
+        {
+            // Arrange
+            _mockNext.Setup(next => next.Invoke(_mockHttpContext.Object)).Verifiable();
+
+            // Act
+            await _exceptionMiddleware.InvokeAsync(_mockHttpContext.Object);
+
+            // Assert
+            _mockNext.Verify();
+        }
+    }
+}
